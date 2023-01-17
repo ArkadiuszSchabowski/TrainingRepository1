e0841d4bd4c2b3c0c7c6b1296bccc78c5e353805
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-17 20:17:50 +0100

    c#+liblary

---
+++ BladZnaleziony/BladZnaleziony/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BladZnaleziony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = ("Świnko");
+            Console.WriteLine("Jak się nazywasz?");
+            UserName = Console.ReadLine();
+            Console.WriteLine("Hey " + UserName);
+            Console.WriteLine("Dobrego dnia " + UserName);
+                }
+    }
+}
