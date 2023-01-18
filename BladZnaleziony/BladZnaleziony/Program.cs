e946a5a9f7a327ff81b7d62d23ac826590a1cd94
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-18 14:30:42 +0100

    new changes

--- BladZnaleziony/BladZnaleziony/Program.cs
+++ BladZnaleziony/BladZnaleziony/Program.cs
@@ -6,11 +6,15 @@ namespace BladZnaleziony
     {
         static void Main(string[] args)
         {
-            string UserName = ("Świnko");
-            Console.WriteLine("Jak się nazywasz?");
-            UserName = Console.ReadLine();
-            Console.WriteLine("Hey " + UserName);
-            Console.WriteLine("Dobrego dnia " + UserName);
-                }
+            int number1, number2;
+
+            Console.WriteLine("Podaj pierwszą liczbę");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Wynik Twojego dodawania to: " + (number1 + number2));
+        }
     }
 }
