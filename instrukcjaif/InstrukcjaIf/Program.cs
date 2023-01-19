e73301c02c2b8d750a5c2a546b0e9fba5a88deee
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-19 22:19:29 +0100

    New files

---
+++ instrukcjaif/InstrukcjaIf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BladZnaleziony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+            double number2;
+
+            Console.WriteLine("Podaj pierwszą liczbę");
+            double.TryParse(Console.ReadLine(), out number1);
+            Console.WriteLine("Podaj drugą liczbę");
+            double.TryParse(Console.ReadLine(), out number2);
+
+
+            Console.WriteLine("Wynik Twojego dodawania to: " + (number1 + number2));
+        }
+    }
+}
