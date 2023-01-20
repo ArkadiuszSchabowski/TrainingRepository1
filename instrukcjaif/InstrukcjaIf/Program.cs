cc1b7882b80b7f45bc34f43b04d0c8347bfbaf9f
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-20 21:04:47 +0100

    true and false

--- instrukcjaif/InstrukcjaIf/Program.cs
+++ instrukcjaif/InstrukcjaIf/Program.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 
-namespace BladZnaleziony
+internal class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int zmienna1, zmienna2;
+        int zmienna1, zmienna2;
 
-            Console.WriteLine("Wprowadź 1 cyfrę");
-            zmienna1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Wprowadź 2 cyfrę");
-            zmienna2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Wynik działania to " + (zmienna1 + zmienna2));
+        Console.WriteLine("Wprowadź 1 cyfrę");
+        zmienna1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Wprowadź 2 cyfrę");
+        zmienna2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Wynik działania to " + (zmienna1 + zmienna2));
 
-            double pierwsza, druga;
+        double pierwsza, druga;
 
-            Console.WriteLine("Wprowadź 3 cyfrę");
-            double.TryParse(Console.ReadLine(), out pierwsza);
-            Console.WriteLine("Wprowadź 4 cyfrę");
-            double.TryParse(Console.ReadLine(), out druga);
-            Console.WriteLine("wynik to: " + (zmienna1 + zmienna2 + pierwsza + druga));
+        Console.WriteLine("Wprowadź 3 cyfrę");
+        pierwsza = double.Parse(Console.ReadLine());
+        Console.WriteLine("Wprowadź 4 cyfrę");
+        druga = double.Parse(Console.ReadLine());
+     
 
-            if ((zmienna1 + zmienna2 + pierwsza + druga) > 20) ;
-
+        if (true)
         }
+    Console.WriteLine("wynik to: " + (zmienna1 + zmienna2 + pierwsza + druga));
     }
 }
