c4116dc3748b20bdc15707239c9488a3ebbd89f8
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-21 14:04:34 +0100

    Swinka branch

--- BladZnaleziony/BladZnaleziony/Program.cs
+++ BladZnaleziony/BladZnaleziony/Program.cs
@@ -5,31 +5,7 @@ namespace BladZnaleziony
     class Program
     {
         static void Main(string[] args)
-
         {
-            bool prawda, nieprawda;
-            int kotek, piesek;
-            string wynik;
-            Console.WriteLine("Podaj pierwszą cyfrę!");
-            kotek = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Podaj drugą cyfrę");
-            piesek = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Wynik Twojego odejmowania to");
-            Console.WriteLine("Twój wynik to " + (kotek - piesek));
-            prawda = kotek == 1;
-            nieprawda = piesek == 0;
-            wynik = C;
-
-            wynik = kotek + piesek;
-            
-
-            if (prawda)
-
-            {
-                Console.WriteLine("Brawo.Poradziłeś, sobie z zadaniem");
-            }
-else
-            Console.WriteLine("Spróbuj jeszcze raz");
 
         }
     }
--- instrukcjaif/InstrukcjaIf/Program.cs
+++ instrukcjaif/InstrukcjaIf/Program.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 
-bool prawda1;
-string prawda2;
+bool x1;
+bool x2;
 
-prawda2 ="C";
+Console.WriteLine("Czy to prawda, że świnka morska była w przeszłości nazywana nadmorską? Wpisz true lub false");
+Console.WriteLine("Czy to prawda, że świnka morska powinna cały czas mieć dostęp do siana? Wpisz true lub false");
 
-Console.WriteLine("Quiz na znajomość świnek morskich");
-Console.WriteLine("Pytanie nr.1. Na czym może ścierać zęby świnka morska");
-Console.WriteLine("A-Banan");
-Console.WriteLine("B-Ogórek");
-Console.WriteLine("C-Siano");
-Console.WriteLine("D-Jabłko");
-Console.ReadLine();
+Boolean.TryParse(Console.ReadLine()), out x1;
+Boolean.TryParse(Console.ReadLine(), out x2);
 
-
-/*if ()
+if (x1 == x2)
 {
-    Console.WriteLine("Brawo.Świnka może zetrzeć zęby na sianie");
+    Console.WriteLine("Brawo.Obie odpowiedzi są poprawne, przechodzisz dalej");
+
+    Console.WriteLine("Quiz na znajomość świnek morskich");
+    Console.WriteLine("Pytanie nr.1. Na czym może ścierać zęby świnka morska");
+    Console.WriteLine("A-Banan");
+    Console.WriteLine("B-Ogórek");
+    Console.WriteLine("C-Siano");
+    Console.WriteLine("D-Jabłko");
 }
 else
 {
     Console.WriteLine("Twoja wiedza o świnkach wymaga powtórki");
 }
-*/
