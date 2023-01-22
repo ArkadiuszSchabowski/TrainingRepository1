bd392c1bfd02f52dcfe591d60b965c1c56340617
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-22 17:35:37 +0100

    New folder Opponent

---
+++ Opponent/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opponent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string y1;
+            int x1, x2;
+
+            Console.WriteLine("Ile zarabiasz rocznie (liczba)?");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ile potrzebujesz zarabiać (liczba)?");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            if (x1 < x2)
+            {
+                Console.WriteLine("Weź się za siebie");
+                Console.ReadLine();
+            }
+            if (x1 == x2)
+            {
+                Console.WriteLine("Zarabiasz dokładnie tyle ile potrzebujesz. Nie stać Cię na więcej?");
+                Console.ReadLine();
+            }
+            if (x1 > x2)
+            {
+                Console.WriteLine("Zarabiasz więcej niż potrzebujesz. Gratulacje");
+                Console.ReadLine();
+            }
+            else
+            {
+            }
+        }
+    }
+}
