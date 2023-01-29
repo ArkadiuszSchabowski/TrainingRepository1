cf218bbe11e485742ab43ef30d0c9d9c44e31035
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-29 14:11:13 +0100

    Dodanie projektu testow xunit i aplikacj WPF

---
+++ Internet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet
+{
+    class players
+    {
+        public string playerName { get; set; }
+        public string playerCountry { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<players> myListOfPlayers = new List<players>();
+
+            foreach (var player in myListOfPlayers)
+            {
+                Console.WriteLine("1");
+                Console.ReadKey();
+            }
+        }
+    }
+}
