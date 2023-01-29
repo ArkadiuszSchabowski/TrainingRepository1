c803a87adce8337dd0ca235951dafe24a96d620a
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-29 20:06:40 +0100

    Koniec na dzis

--- BirthayProject/Program.cs
+++ BirthayProject/Program.cs
@@ -8,7 +8,7 @@ namespace PrawidlowyKod
 {
     internal class Operacja
     {
-        static void Main(string[] args)
+        public static void StartMenu()
         {
             while (true)
             {
