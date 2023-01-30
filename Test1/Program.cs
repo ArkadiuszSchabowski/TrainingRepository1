b7c5084c12fbf12b181efbfc4632ce5adfc6eb84
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-30 19:22:20 +0100

    cwiczenia

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -13,24 +13,37 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            int szerokosc, dlugosc;
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
 
-            Console.WriteLine("Wprowadź szerokosc prostokąta");
-            szerokosc = int.Parse(Console.ReadLine());
-            Console.WriteLine("Wprowadź długość prostokąta");
-            dlugosc = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Szerokosc Twojego prostokąta to: " + szerokosc + "." + " Dlugosc Prostokoąta to: " + dlugosc + ".");
-
-            for (int i = 0; i < dlugosc; i++)
+            while (true)
             {
-                for (int j = 0; j < szerokosc; j++)
+                Console.Write("Please fancy your value: ");
+                int yourNumber;
+                bool yourNumber_result = int.TryParse(Console.ReadLine(), out yourNumber);
+
+                if (yourNumber_result == true)
                 {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.Write("*");
+                    if (yourNumber == randomNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Congratulations!!! You have guessed the number!!! ");
+                        break;
+                    }
+                    else if (yourNumber < randomNumber)
+                    {
+                        Console.WriteLine("The number is greater than " + yourNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is less than " + yourNumber);
+                    }
                 }
-                Console.WriteLine();
+                else
+                {
+                    Console.WriteLine("You have added incorrect value");
+                }
+
             }
             Console.ReadKey();
         }
