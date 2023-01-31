f9318957a3d4f8f011a5314bd158d0ee3e3e606c
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-01-31 19:29:10 +0100

    31 january exercises

---
+++ 31Styczeń/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31Styczeń
+{
+    internal class Program
+    {
+        static void NumbersInCSharp()
+        {
+            int a = 10;
+            int b = 5;
+
+            Console.WriteLine("Wprowadź pierwszą liczbę ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Twoja liczba to: {a}!");
+
+            Console.WriteLine("Wprowadź drugą liczbę ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Twoja liczba to: {b}!");
+
+            Console.WriteLine($"Wynik mnożenia dwóch podanych przez Ciebie liczb to: {a * b}");
+
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            NumbersInCSharp();
+        }
+    }
+}
---
+++ 8k/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _8k
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hej Ekipa z Kursu 8k.";
+            Console.WriteLine(hello.StartsWith("Hello"));
+            Console.WriteLine(hello.EndsWith("6k."));
+            Console.ReadKey();
+        }
+    }
+}
