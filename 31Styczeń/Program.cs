86607756a8a43a0a059f10cbe5732e0711a16984
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-01 18:25:35 +0100

    fourth base

--- 31Styczeń/Program.cs
+++ 31Styczeń/Program.cs
@@ -1,29 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _31Styczeń
 {
     internal class Program
-//Teraz, gdy znasz już różne typy liczbowe, napisz kod obliczający pole koła o danym promieniu - załóżmy w naszym przykladzie, że bedzie to` 2,50 centymetra.Pamiętaj, że obszar koła to promień podniesiony do kwadratu pomnożony przez PI.
     {
-        static void doubleradius()
-        {
-
-            double r = 2.5*2.5;
-            double pi = Math.PI;
-            double kolo = r * pi;
-
-            Console.WriteLine($"Pole koła wynosi: {kolo}!");
-            Console.ReadKey();
-
-        }
-        
         static void Main(string[] args)
         {
-        doubleradius();
+            czwartaBaza();
+        }
+  
+        static void czwartaBaza()
+        //Napisz przy użyciu pętli oraz instrukcji if kod, który znajdzie sumę wszystkich 
+        //liczb całkowitych od 1 do 30, które są podzielne przez 7. Wskazówka - użyj modulo.
+        {
+            int x1, x2, x3, x4, wynik;
+            for (int i = 1; i <= 30; i++)
+
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine((i));
+                }
+            Console.WriteLine("Oto liczby, które są podzielne przez 7.\nAby uzyskać sumę tych liczb wprowadź wyświetlone liczby:");
+            Console.Write("Przepisz 1 liczbę ");
+            x1 = int.Parse(Console.ReadLine());
+            Console.Write("Przepisz 2 liczbę ");
+            x2 = int.Parse(Console.ReadLine());
+            Console.Write("Przepisz 3 liczbę ");
+            x3 = int.Parse(Console.ReadLine());
+            Console.Write("Przepisz 4 liczbę ");
+            x4 = int.Parse(Console.ReadLine());
+
+            wynik = x1 + x2 + x3 + x4;
+            Console.WriteLine($"Twój wynik to: {wynik}.");
+            Console.ReadKey();
         }
     }
 }
