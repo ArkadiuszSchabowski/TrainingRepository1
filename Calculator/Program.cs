8fd82f97dfd3fdd24202a3becdeb2092cf06b037
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-01 22:05:10 +0100

    old habits deleted for life

---
+++ Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine();
+        }
+
+        static void Engine()
+        {
+            double x1, x2;
+            string operation;
+
+            Console.WriteLine("Aplikacja konsolowy kalkulator\n");
+
+            while (true)
+            {
+                Console.WriteLine("Podaj pierwszą liczbę");
+                double.TryParse(Console.ReadLine(), out x1);
+                Console.WriteLine("Podaj drugą liczbę");
+                double.TryParse(Console.ReadLine(), out x2);
+                Console.WriteLine("Podaj operację, którą chcesz wykonać.\nDostępne operacje: dodaj, odejmij, pomnóż, podziel");
+                Console.WriteLine("");
+                operation = Console.ReadLine();
+                Console.WriteLine("");
+
+                switch (operation)
+                {
+                    case "dodaj":
+                        Console.WriteLine($"Twój wynik to: {x1 + x2}\n");
+                        break;
+                    case "odejmij":
+                        Console.WriteLine($"Twój wynik to: {x1 - x2}\n");
+                        break;
+                    case "pomnóż":
+                        Console.WriteLine($"Twój wynik to: {x1 * x2}\n");
+                        break;
+                    case "podziel":
+                        Console.WriteLine($"Twój wynik to: {x1 / x2}\n");
+                        break;
+                    default:
+                        Console.WriteLine("Podałeś złą operację");
+                        break;
+                }
+            }
+        }
+
+        static void GetInput()
+        {
+
+        }
+    }
+}
+
---
+++ MenuWindowsForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("MenuWindowsForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MenuWindowsForm")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("2d6bea5c-754c-43e8-8916-19eca0b83905")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
