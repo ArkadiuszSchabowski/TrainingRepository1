5c75dd3bb40cc1aca3652bddc1c58821ac9bbb40
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-03 15:10:36 +0100

    fifth base project list<T>

---
+++ FifthBase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Arkadiusz", "Anielka", "Felix" };
+            Console.WriteLine();
+            names.Add("Arkadiusz");
+            names.Add("Bill");
+            names.Remove("Anielka");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+                Console.WriteLine($"My name is {names[0]}");
+                Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+                Console.ReadKey();
+            }
+        }
+    }
+}
