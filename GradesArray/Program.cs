89e6a0a2cd0fa26c59940d690e8b1f3ecc35fae2
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-08 22:23:03 +0100

    Arrays work

---
+++ GradesArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] gradesArray = new int[10] { 5, 6, 2, 1, 3, 4, 4, 5, 2, 1 };
+            for (int i = 0; i < gradesArray.Length; i++)
+            {
+                Console.WriteLine("index i " + i + " pozycja w tablicy " + (i + 1) + " ocena " + gradesArray[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
