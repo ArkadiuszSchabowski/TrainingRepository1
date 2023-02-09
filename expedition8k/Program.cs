fa8f724ada2092e2995562a81927fdd3056df175
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-09 12:51:57 +0100

    Exercises 3 upload to fbgroup

--- expedition8k/Program.cs
+++ expedition8k/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,24 +12,31 @@ namespace expedition8k
 {
     internal class Program
     {
-        static void WorkWithIntegers()
-        {
+        //Teraz, gdy znasz już różne typy liczbowe, napisz kod obliczający pole koła o danym promieniu - załóżmy w naszym przykladzie, że bedzie to` 2,50 centymetra.Pamiętaj, że obszar koła to promień podniesiony do kwadratu pomnożony przez PI.
 
-            int a = 10;
-            int b = 20;
-            int c = a + b;
-            Console.WriteLine($"Twój wynik to {c}");
+        //.NET zawiera stałą dla PI, Math.PI, której można użyć dla tej wartości. Math.PI, podobnie jak wszystkie stałe zadeklarowane w przestrzeni nazw System.Math, jest wartością podwójną.
+        //W tym zadaniunależy używać wartości double zamiast wartości int. Powinieneś otrzymać odpowiedź między 19 a 20.
 
-            Console.ReadKey();
-        }
+        //Teraz przenieś kod do osobnej metody z parametrem "double radius" i koniecznie podziel się swoim kodem w naszej grupie na FB
         static void Main(string[] args)
         {
-            double a, b, c;
-            a = 10;
-            b = 3;
-            c = a / b;
+            DoubleRadius();
+        }
+        static void DoubleRadius()
+        {
+            double r, obszarKola, poleKola, pi;
 
-            Console.WriteLine(Math.Round(c, 2));
+            r = 2.5;
+            obszarKola = r * r;
+            pi = Math.PI;
+            poleKola = obszarKola * pi;
+
+            //Console.WriteLine("Pole naszego koła po zaokrągleniu wynosi: " + Math.Round(poleKola, 2));
+            ////Zapis numer1
+
+            Console.WriteLine($"Pole naszego koła po zaokrągleniu wynosi: {Math.Round(poleKola, 2)}");
+            //Zapis numer2
+
             Console.ReadKey();
         }
     }
