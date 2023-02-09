dc66f0cb79e647488bf1cb4c817e943ea22ac47b
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-09 11:19:13 +0100

    Expedition - expedition8k.cs

---
+++ expedition8k/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expedition8k
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello;
+
+            hello = "Hej Ekipa";
+
+            hello = hello.Replace("Hej", "Good Morning");
+            hello = hello.Replace("Ekipa", "Team");
+            Console.WriteLine(hello);
+
+            Console.ReadKey();
+        }
+    }
+}
