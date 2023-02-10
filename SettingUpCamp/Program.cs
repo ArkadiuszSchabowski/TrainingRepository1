8636f0d184af1223290be07d5803883bc0d23a87
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-10 21:14:11 +0100

    Fibonnaci string - done. (SettingUpCamp.cs)

---
+++ SettingUpCamp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingUpCamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            var sum = 0;
+
+            for (int i = 0; i < 18; i++)
+            {
+
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+            Console.WriteLine("Twoje liczby ciągu Fibonacciego to kolejno:\n");
+
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.WriteLine(item);
+                sum = sum + item;
+            }
+            Console.WriteLine($"\nSuma ciągu Fibonacciego 20 liczb wynosi: {sum}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SherpaLoop'a/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SherpaLoop_a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int i = 1;
+
+            while (i <= 30)
+            {
+                if(i % 7 == 0)
+                {
+                    sum = sum + i;
+                }
+                i++;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
+
