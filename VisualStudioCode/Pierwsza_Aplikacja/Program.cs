06c49d35ea8aa4b8c166b42bf89c92990eeb9013
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-16 19:58:11 +0100

    Visual Studio instaled

---
+++ VisualStudioCode/Pierwsza_Aplikacja/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+String s = "5";
+int i = 5;
+System.Console.WriteLine(s + i);
+int b;
+b = int.Parse(s);
+System.Console.WriteLine(b + i);
