aaad8fbc5e361e6a15c0ac8803deb9dc44d7a973
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-17 16:53:32 +0100

    ladder

--- Learn.cs
+++ Learn.cs
@@ -1,35 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
-using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SzybkiProject
+namespace LearrnEtc
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             int wysokoscDrabiny;
-            Console.WriteLine("Podaj wysokość drabiny");
+            Console.WriteLine("Podaj szerokość drabiny");
             wysokoscDrabiny = int.Parse(Console.ReadLine());
+
             for (int i = 0; i < wysokoscDrabiny; i++)
             {
                 for (int j = 0; j < wysokoscDrabiny; j++)
                 {
-                    if (i%2==1)
+                    if (i % 2 == 0)
                     {
-                        Console.Write("X");
+                        Console.Write("*");
                     }
                     else
                     {
                         Console.Write(".");
                     }
                 }
-                Console.WriteLine();
+                Console.WriteLine("");
             }
             Console.ReadKey();
         }
