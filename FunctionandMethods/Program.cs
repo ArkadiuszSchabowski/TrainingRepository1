7fedd3f8936aeac7e8e8d82715456a4e87717930
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-19 20:50:29 +0100

    MethodsandFuctions Learning

---
+++ FunctionandMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionandMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(2, 4);
+            //Addition(2, 4);
+            Console.ReadKey();
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Addition(int a, int b)
+        {
+            int wynik = a + b;
+            Console.WriteLine($"Your result is: {wynik}");
+
+
+        }
+    }
+}
