6aa3d2f1e63ddbc4c3fa50bf5b290e94e19c4fb6
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-25 11:53:31 +0100

    6 week done✔

--- LiczbyPierwsze/Program.cs
+++ LiczbyPierwsze/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LiczbyPierwsze
 {
@@ -10,6 +14,18 @@ namespace LiczbyPierwsze
     {
         static void Main(string[] args)
         {
+            int[] n = new int[10] { 3, 4, 2, 1, 6, 5, 4, 3, 2, 1 };
+            int sum = 0;
+            int srednia = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i]);
+                sum += n[i];
+            }
+            Console.WriteLine($"Suma ocen w tablicy to {sum}.");
+            srednia = sum / n.Length;
+            Console.WriteLine($"Twoja suma ocen w tablicy to {srednia}");
+            Console.ReadKey();
         }
     }
 }
