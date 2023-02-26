75353bd28c32909536e60705e3018c166c6bc8a0
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-26 19:41:54 +0100

    Average in List✔

---
+++ NormalList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormalList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 5, 2, 3 };
+            int numbersSum = 0;
+
+            foreach (int number in numbers)
+            {
+                numbersSum += number;
+            }
+
+            double numbersAverage = (double)numbersSum / numbers.Length;
+            Console.WriteLine(numbersAverage);
+            Console.ReadKey();
+
+            //List<int> numbersList = new List<int>() { 4, 5, 2, 3 };
+            //double listAverage = numbersList.Average();
+            //Console.WriteLine(listAverage);
+            //Console.ReadKey();
+        }
+    }
+}
