fded3eef36429e17bf43f6f5a93be9c82dc06411
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-02-28 21:11:02 +0100

    TicTacToe ended 🍺

--- TicTacToeGame/MainWindow.xaml.cs
+++ TicTacToeGame/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace TicTacToeGame
     public partial class MainWindow : Window
     {
         public bool isPlayer1Turn { get; set; } = true;
+
+        public int Counter { get; set; }
         public MainWindow()
         {
             InitializeComponent();
@@ -30,17 +32,113 @@ namespace TicTacToeGame
         }
         public void NewGame()
         {
+            isPlayer1Turn = false;
+            Counter = 0;
 
+            Button_0_0.Content = string.Empty;
+            Button_1_0.Content = string.Empty;
+            Button_2_0.Content = string.Empty;
+            Button_0_1.Content = string.Empty;
+            Button_1_1.Content = string.Empty;
+            Button_2_1.Content = string.Empty;
+            Button_0_2.Content = string.Empty;
+            Button_1_2.Content = string.Empty;
+            Button_2_2.Content = string.Empty;
+
+            Button_0_0.Background = Brushes.White;
+            Button_1_0.Background = Brushes.White;
+            Button_2_0.Background = Brushes.White;
+            Button_0_1.Background = Brushes.White;
+            Button_1_1.Background = Brushes.White;
+            Button_2_1.Background = Brushes.White;
+            Button_0_2.Background = Brushes.White;
+            Button_1_2.Background = Brushes.White;
+            Button_2_2.Background = Brushes.White;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             isPlayer1Turn ^= true;
+            Counter++;
 
+            if (Counter > 9)
+            {
+                NewGame();
+                return;
+            }
             var button = sender as Button;
 
             button.Content = isPlayer1Turn ? "O" : "X";
-        }
 
+            if (CheckIfPlayerWon())
+            {
+                Counter = 9;
+            }
+            else
+            {
+
+            }
+        }
+        private bool CheckIfPlayerWon()
+        {
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_0_1.Content && Button_0_1.Content == Button_0_2.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_0_1.Background = Brushes.Green;
+                Button_0_2.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_1_0.Content.ToString() != string.Empty && Button_1_0.Content == Button_1_1.Content && Button_1_1.Content == Button_1_2.Content)
+            {
+                Button_1_0.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_1_2.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_2_0.Content.ToString() != string.Empty && Button_2_0.Content == Button_2_1.Content && Button_2_1.Content == Button_2_2.Content)
+            {
+                Button_2_0.Background = Brushes.Green;
+                Button_2_1.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+
+            }
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_1_0.Content && Button_1_0.Content == Button_2_0.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_1_0.Background = Brushes.Green;
+                Button_2_0.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_0_1.Content.ToString() != string.Empty && Button_0_1.Content == Button_1_1.Content && Button_1_1.Content == Button_2_1.Content)
+            {
+                Button_0_1.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_1.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_0_2.Content.ToString() != string.Empty && Button_0_2.Content == Button_1_2.Content && Button_1_2.Content == Button_2_2.Content)
+            {
+                Button_0_2.Background = Brushes.Green;
+                Button_1_2.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_0_0.Content.ToString() != string.Empty && Button_0_0.Content == Button_1_1.Content && Button_1_1.Content == Button_2_2.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_0_2.Content.ToString() != string.Empty && Button_0_2.Content == Button_1_1.Content && Button_1_1.Content == Button_2_0.Content)
+            {
+                Button_0_2.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_0.Background = Brushes.Green;
+                return true;
+            }
+            return false;
+        }
     }
 }
