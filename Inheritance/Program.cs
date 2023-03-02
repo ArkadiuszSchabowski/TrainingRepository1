334d25e9f55ec4126ecd2012c241d72376dba50f
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-03-02 15:23:03 +0100

    Dziedziczenie✔

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            Mazda mazda = new Mazda();
+            mazda.Pochodzenie();
+            mazda.IloscKol();
+            mazda.Kolor();
+            Console.ReadKey();
+        }
+    }
+    abstract class Samochod
+    {
+        public void Kolor()
+        {
+            Console.WriteLine("Każdy samochód jest jakiegoś koloru");
+        }
+    }
+    abstract class SamochodOsobowy : Samochod
+    {
+        public void IloscKol()
+        {
+            Console.WriteLine("Każdy samochód osobowy ma 4 koła");
+        }
+    }
+    class Mazda : SamochodOsobowy
+    {
+        public void Pochodzenie()
+        {
+            Console.WriteLine("Samochód Mazda wywodzi się z Japonii");
         }
     }
 }
