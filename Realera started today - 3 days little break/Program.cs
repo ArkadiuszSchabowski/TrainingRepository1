49e5ec8665575bd4ef692894ac38115d3775ccad
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-03-03 14:52:25 +0100

    Realera started

---
+++ Realera started today - 3 days little break/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realera_started_today___3_days_little_break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //One day break
+        }
+    }
+}
