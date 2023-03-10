a4eebce8a686ff2b7921d55c1585ed4854345b62
Arkadiusz Schabowski <arkadiuszschabowski@gmail.com>
2023-03-10 09:45:53 +0100

    struct

---
+++ Struktury/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struktury
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt2D punkt1;
+            punkt1.x = 5;
+
+            Console.WriteLine(punkt1.x);
+            Console.ReadKey();
+        }
+    }
+    struct Punkt2D
+    {
+        public int x;
+        public int y;
+        public void OdlegloscOdSrodka()
+        {
+
+        }
+    }
+}
