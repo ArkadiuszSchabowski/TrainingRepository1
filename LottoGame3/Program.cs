389fb8c97dfff856e26cfc77774e445ae9d00f2f
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-14 21:11:44 +0100

    day 1 ended

--- LottoGame3/Program.cs
+++ LottoGame3/Program.cs
@@ -36,8 +36,8 @@ namespace LottoGame
                         Console.Clear();
                         Console.WriteLine("Dzień {0}", dzien);
                         Console.WriteLine("Witaj w grze Lotto dziś do wygrania aż {0}zł!", kumulacja);
-                        Console.WriteLine("Stan konta {0}zł", pieniadze);
-                        WyswietlKupon();
+                        Console.WriteLine("\nStan konta {0}zł\n", pieniadze);
+                        WyswietlKupon(kupon);
                         //Menu
                         if (pieniadze >= 3 && losow <= 8)
                         {
@@ -53,25 +53,30 @@ namespace LottoGame
                             pieniadze -= 3;
                             losow++;
                         }
-                        else if (wybor == ConsoleKey.D2)
-                        {
-                            SprawdzKupon();
-                        }
-                        else if (wybor == ConsoleKey.D3 || wybor == ConsoleKey.Escape)
-                        {
-
-                        }
                     } while (wybor == ConsoleKey.D1);
                     Console.Clear();
                     if (kupon.Count > 0)
                     {
-
+                        int wygrana = Sprawdz(kupon);
+                        if (wygrana > 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Brawo wygrałeś {0}zł w tym losowaniu!", wygrana);
+                            Console.ResetColor();
+                            pieniadze += wygrana;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Niestety, nic nie wygrałeś");
+                            Console.ResetColor();
+                        }
                     }
                     else
                     {
                         Console.WriteLine("Nie miałeś losów w tym losowaniu!");
                     }
-                    Console.WriteLine("Enter - kontynuuj");
+                    Console.WriteLine("Enter - kontynuuj.");
                     Console.ReadKey();
                 } while (pieniadze >= 3 && wybor != ConsoleKey.D3);
                 Console.Clear();
@@ -80,19 +85,66 @@ namespace LottoGame
             } while (Console.ReadKey().Key == ConsoleKey.Enter);
         }
 
-        private static void SprawdzKupon()
+        private static int Sprawdz(List<int[]> kupon)
         {
             throw new NotImplementedException();
         }
 
         private static int[] PostawLos()
         {
-            throw new NotImplementedException();
+            int[] liczby = new int[6];
+            int liczba = -1;
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                liczba = -1;
+                Console.Clear();
+                Console.Write("Postawione liczby: ");
+                foreach (int l in liczby)
+                {
+                    if (l > 0)
+                    {
+                        Console.WriteLine(l + ", ");
+                    }
+                }
+                Console.WriteLine("\n\nWybierz liczby od 1 do 49:");
+                Console.Write("{0}/6: ", i + 1);
+                bool prawidlowa = int.TryParse(Console.ReadLine(), out liczba);
+                if (prawidlowa && liczba >= 1 && liczba <= 49 && !liczby.Contains(liczba))
+                {
+                    liczby[i] = liczba;
+                }
+                else
+                {
+                    Console.WriteLine("Niestety błędna liczba");
+                    i--;
+                    Console.ReadKey();
+                }
+            }
+            Array.Sort(liczby);
+            return liczby;
         }
 
-        private static void WyswietlKupon()
+        private static void WyswietlKupon(List<int[]> kupon)
         {
-            throw new NotImplementedException();
+            if (kupon.Count == 0)
+            {
+                Console.WriteLine("\nNie postawiłeś jeszcze żadnych losów");
+            }
+            else
+            {
+                int i = 0;
+                Console.WriteLine("\nTwój kupon:");
+                foreach (int[] los in kupon)
+                {
+                    i++;
+                    Console.WriteLine(i + ": ");
+                    foreach (int liczba in los)
+                    {
+                        Console.Write(liczba + ",");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
