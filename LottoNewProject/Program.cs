10d78ecaaec1f190efeafbae89f96913e88ca465
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-15 22:51:19 +0100

    LottoGame training skills✔

--- LottoNewProject/Program.cs
+++ LottoNewProject/Program.cs
@@ -12,22 +12,58 @@ namespace LottoNewProject
         static void Main(string[] args)
         {
             PodajLiczby();
+            WylosujLiczby();
+            SprawdzLiczby();
         }
+
+        private static void SprawdzLiczby()
+        {
+        }
+
+        private static void WylosujLiczby()
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(1, 50);
+            int[] computerNumbers = new int[6];
+
+
+            for (int  i = 0;  i < computerNumbers.Length;  i++)
+            {
+                int los = rnd.Next(1, 50);
+                if (!computerNumbers.Contains(los))
+                {
+                    computerNumbers[i] = los;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            Array.Sort(computerNumbers);
+            Console.WriteLine("\n\nLiczby wylosowane przez komputer to: ");
+            foreach (var item in computerNumbers)
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
         static void PodajLiczby()
         {
-            int[] liczby = new int[6];
-            Console.WriteLine("Podaj 6 liczb z zakresu 1-49");
+            int[] userNumbers = new int[6];
+            Console.WriteLine("Podaj 6 liczb z zakresu 1-49\n");
 
-            for (int i = 0; i < liczby.Length; i++)
+            for (int i = 0; i < userNumbers.Length; i++)
             {
                 bool result = true;
                 int number;
 
                 Console.WriteLine("Podaj liczbę {0}:", i +1);
                 result = int.TryParse(Console.ReadLine(), out number);
-                if (result && (number > 0 && number < 50) && !liczby.Contains(number))
+                if (result && (number > 0 && number < 50) && !userNumbers.Contains(number))
                 {
-                    liczby[i] = number;
+                    userNumbers[i] = number;
                 }
                 else
                 {
@@ -35,13 +71,14 @@ namespace LottoNewProject
                     i--;
                 }
             }
-            Array.Sort(liczby);
+            Array.Sort(userNumbers);
             Console.WriteLine();
-            foreach (var item in liczby)
+            Console.WriteLine("Twoje liczby to:");
+            foreach (var item in userNumbers)
             {
                 Console.Write(item + ",");
             }
-            Console.ReadKey();
+            Console.WriteLine();
         }
     }
 }
