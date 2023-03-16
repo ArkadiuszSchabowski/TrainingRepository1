8b1842e45a03cd368e4186371723ea5387fb10ac
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-16 21:35:52 +0100

    Solo Project

---
+++ ThisIsCrazy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsCrazy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Zgadnij 6 liczb z przedziału 1-49. Przewidziane atrakcyjne nagrody");
+            bool result = int.TryParse(Console.ReadLine(), out number);
+            if (result && (number>0 && number < 50))
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Podałeś złą liczbę!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
