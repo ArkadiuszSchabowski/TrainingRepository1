6af422136b4ae6be7917ba355351ec0fcb96b4b8
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-16 21:58:27 +0100

    22:00 push

--- ThisIsCrazy/Program.cs
+++ ThisIsCrazy/Program.cs
@@ -8,21 +8,59 @@ namespace ThisIsCrazy
 {
     public class Program
     {
+        int kumulacja;
         static void Main(string[] args)
         {
-            int number = 0;
-            Console.WriteLine("Zgadnij 6 liczb z przedziału 1-49. Przewidziane atrakcyjne nagrody");
-            bool result = int.TryParse(Console.ReadLine(), out number);
-            if (result && (number>0 && number < 50))
+            //UserNumbers();
+            ComputerNumbers();
+        }
+
+        private static void ComputerNumbers()
+        {
+            int[] computerNumbers = new int[6];
+            int number;
+            Random rnd = new Random();
+
+            for (int i = 0; i < computerNumbers.Length; i++)
             {
-                Console.WriteLine(number);
+                number = rnd.Next(1, 50);
+                computerNumbers[i] = number;
             }
-            else
+
+            Array.Sort(computerNumbers);
+            foreach (var item in computerNumbers)
             {
-                Console.WriteLine("Podałeś złą liczbę!");
+                Console.WriteLine(item);
             }
             Console.ReadKey();
+        }
 
+        private static void UserNumbers()
+        {
+            int number = 0;
+            int[] userNumbers = new int[6];
+
+            Console.WriteLine("Zgadnij 6 liczb z przedziału 1-49. Przewidziane atrakcyjne nagrody");
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                bool result = int.TryParse(Console.ReadLine(), out number);
+                if (result && (number > 0 && number < 50) && !userNumbers.Contains(number))
+                {
+                    userNumbers[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine("Podałeś złą wartość!");
+                    i--;
+                }
+            }
+            Array.Sort(userNumbers);
+            Console.WriteLine();
+            foreach (var item in userNumbers)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
+
