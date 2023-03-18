4fc3ee4f86c2d15d5f47ddfcaf00df67245cead3
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-18 17:03:06 +0100

    Skills training Lotto

--- JedenKupon/Program.cs
+++ JedenKupon/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,61 +11,77 @@ namespace JedenKupon
     {
         static Random rnd = new Random();
         static int kumulacja;
+        static int start = 50;
         static int pieniadze = 50;
-        static int dzien = 1;
+        static int dzien = 0;
         static void Main(string[] args)
         {
-            int[] kupon = new int[6];
-            int losow = 0;
             kumulacja = rnd.Next(2, 37) * 1000000;
-
+            int[] kupon = new int[6];
+            bool flaga = true;
+            int losow = 0;
+            ConsoleKey wybor2;
             do
             {
-                Console.Clear();
-                Console.WriteLine("To Twój {0} dzień gry", dzien);
-                Console.WriteLine("Twój stan portfela to: {0}zł.", pieniadze);
-                Console.WriteLine($"Dzisiejsza kumulacja wynosi: {kumulacja}!");
-                Console.WriteLine();
-                if (losow == 0)
+                losow = 0;
+                dzien = 1;
+                pieniadze = start;
+
+                do
                 {
-                    Console.WriteLine("Nie postawiłeś jeszcze żadnych losów.\n");
-                }
-                else
-                {
-                    Console.WriteLine("Postawiłeś los. Postawionych losów: {0}/1.\n", losow);
-                    WyswietlKupon(kupon);
-                }
-
-                Console.WriteLine("1 - Postaw kupon");
-                Console.WriteLine("2 - Sprawdz Kupon");
-                Console.WriteLine("3 - Zakoncz grę");
-
-                ConsoleKey wybor = Console.ReadKey().Key;
-
-                if (wybor == ConsoleKey.D1 && losow < 1 && pieniadze >= 3)
-                {
-                    int[] kupon1 = PostawKupon(kupon);
-                    pieniadze -= 3;
-                    losow++;
-
-                    foreach (var item in kupon1)
+                    Console.Clear();
+                    Console.WriteLine("To Twój {0} dzień gry", dzien);
+                    Console.WriteLine("Twój stan portfela to: {0}zł.", pieniadze);
+                    Console.WriteLine($"Dzisiejsza kumulacja wynosi: {kumulacja}!");
+                    Console.WriteLine();
+                    if (losow == 0)
                     {
-                        Console.Write(item + ", ");
+                        Console.WriteLine("Nie postawiłeś jeszcze żadnych losów.\n");
                     }
-                    Console.ReadKey();
-                }
-                if (wybor == ConsoleKey.D2)
-                {
-                    dzien++;
-                    int[] kupon2 = WylosowaneNumery(kupon);
-                    Sprawdz(kupon, kupon2);
-                }
-                if (wybor == ConsoleKey.D3)
-                {
-                    Console.WriteLine("Zrezygnuj z dalszej gry");
-                }
-            } while (true);
+                    else
+                    {
+                        Console.WriteLine("Postawiłeś los. Postawionych losów: {0}/1.\n", losow);
+                        WyswietlKupon(kupon);
+                    }
 
+                    Console.WriteLine("1 - Postaw kupon");
+                    Console.WriteLine("2 - Sprawdz Kupon");
+                    Console.WriteLine("3 - Zakoncz grę");
+
+                    ConsoleKey wybor = Console.ReadKey().Key;
+
+                    if (wybor == ConsoleKey.D1 && losow < 1 && pieniadze >= 3)
+                    {
+                        int[] kupon1 = PostawKupon(kupon);
+                        pieniadze -= 3;
+                        losow++;
+
+                        foreach (var item in kupon1)
+                        {
+                            Console.Write(item + ", ");
+                        }
+                        Console.ReadKey();
+                    }
+                    if (wybor == ConsoleKey.D2)
+                    {
+                        dzien++;
+                        int[] wylosowane = WylosowaneNumery(kupon);
+                        Sprawdz(kupon, wylosowane);
+                    }
+                    if (wybor == ConsoleKey.D3)
+                    {
+                        flaga = false;
+                    }
+                } while (flaga == true);
+
+                Console.Clear();
+                Console.WriteLine($"Zakończyłeś grę w {dzien} dzien");
+                Console.WriteLine($"Twój wynik to: {pieniadze - start}zł");
+                Console.WriteLine("Nacisnij Enter, by zagrac jeszcze raz");
+
+                wybor2 = Console.ReadKey().Key;
+
+            } while (wybor2 == ConsoleKey.Enter);
         }
 
         private static void WyswietlKupon(int[] kupon1)
@@ -107,11 +124,12 @@ namespace JedenKupon
         }
         private static int[] WylosowaneNumery(int[] kupon)
         {
+
             int[] computerNumbers = new int[6];
 
             for (int i = 0; i < computerNumbers.Length; i++)
             {
-                int number = rnd.Next(1, 50);
+                int number = rnd.Next(1, 11);
                 if (!computerNumbers.Contains(number))
                 {
                     computerNumbers[i] = number;
@@ -124,15 +142,14 @@ namespace JedenKupon
             Array.Sort(computerNumbers);
             return computerNumbers;
         }
-        private static int[] Sprawdz(int[] kupon1, int[] kupon2)
+        private static void Sprawdz(int[] kupon1, int[] kupon2)
         {
-            int[] trafione = new int[6];
-            trafione[0] = 0;
-            trafione[1] = 0;
-            trafione[2] = 24;
-            trafione[3] = 250;
-            trafione[4] = 5000;
-            trafione[5] = kumulacja;
+            int i = 0;
+            int j = 0;
+
+            int wygrana3 = 24;
+            int wygrana4 = 250;
+            int wygrana5 = 5000;
 
             Console.Clear();
             Console.WriteLine($"LIczby z kuponu użytkownika to:");
@@ -146,9 +163,73 @@ namespace JedenKupon
             {
                 Console.Write(item2 + ", ");
             }
+            do
+            {
+                if (kupon1[0] == kupon2[i])
+                {
+                    j++;
+                }
+
+                else if (kupon1[1] == kupon2[i])
+                {
+                    j++;
+                }
+                else if (kupon1[2] == kupon2[i])
+                {
+                    j++;
+                }
+                else if (kupon1[3] == kupon2[i])
+                {
+                    j++;
+                }
+                else if (kupon1[4] == kupon2[i])
+                {
+                    j++;
+                }
+                else if (kupon1[5] == kupon2[i])
+                {
+                    j++;
+                }
+                i++;
+            } while (i < kupon1.Length);
+            Console.WriteLine();
+            Console.WriteLine();
+            if (j == 6)
+            {
+                Console.WriteLine($"Trafiłeś {j} liczb!");
+                Console.WriteLine($"Wygrałeś {kumulacja}zł! Gratulacje!");
+                pieniadze += kumulacja;
+            }
+            else if (j == 5)
+            {
+                Console.WriteLine($"Trafiłeś {j} liczb!");
+                Console.WriteLine($"Wygrałeś {wygrana5}zł! Gratulacje!");
+                pieniadze += wygrana5;
+            }
+            else if (j == 4)
+            {
+                Console.WriteLine($"Trafiłeś {j} liczby!");
+                Console.WriteLine($"Wygrałeś {wygrana4}zł! Gratulacje!");
+                pieniadze += wygrana4;
+            }
+            else if (j == 3)
+            {
+                Console.WriteLine($"Trafiłeś {j} liczby!");
+                Console.WriteLine($"Wygrałeś {wygrana3}zł! Gratulacje!");
+                pieniadze += wygrana3;
+            }
+            else if (j == 2)
+            {
+                Console.WriteLine($"Trafiłeś {j} liczby!");
+                Console.WriteLine("Niestety nic nie wygrałeś!");
+            }
+            else
+            {
+                Console.WriteLine($"Trafiłeś {j} liczb!");
+                Console.WriteLine("Niestety nic nie wygrałeś!");
+            }
 
             Console.ReadKey();
-            return trafione;
         }
     }
 }
