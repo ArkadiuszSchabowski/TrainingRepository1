38bc8183c9fe1ca368147d3a237308120eedfcde
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-22 22:57:28 +0100

    Day ended

--- SwineczkaProgramujeKalkulator/Form1.cs
+++ SwineczkaProgramujeKalkulator/Form1.cs
@@ -2,6 +2,7 @@ namespace SwineczkaProgramujeKalkulator
 {
     public enum Operation
     {
+        None,
         Addition,
         Substraction,
         Multiplication,
@@ -9,9 +10,11 @@ namespace SwineczkaProgramujeKalkulator
     }
     public partial class Form1 : Form
     {
-        public string _firstValue;
-        public string _secondValue;
-        public string _currentOperation;
+        private string _firstValue;
+        private string _secondValue;
+        private Operation _currentOperation = Operation.None;
+        private bool _isTheResultOnTheScreen;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,26 +26,115 @@ namespace SwineczkaProgramujeKalkulator
 
             var clickedValue = (sender as Button).Text;
 
-            if (tbScreen.Text == "0")
-                tbScreen.Clear();
+            if (tbScreen.Text == "0" && clickedValue != ",")
+                tbScreen.Text = string.Empty;
+
+            if (_isTheResultOnTheScreen)
+            {
+                _isTheResultOnTheScreen = false;
+                tbScreen.Text = string.Empty;
+            }
+
+            if (clickedValue == "0")
+            {
+                if (clickedValue == ",")
+                {
+                    clickedValue = "0,";
+                }
+            }
 
             tbScreen.Text += clickedValue;
+            SetResultBtnState(true);
+
+            if (_currentOperation != Operation.None)
+                _secondValue += clickedValue;
+            else
+            SetOperationBtnState(true);
 
         }
 
         private void OnBtnOperationClick(object sender, EventArgs e)
         {
+            _firstValue = tbScreen.Text;
 
+            var operation = (sender as Button).Text;
+
+            _currentOperation = operation switch
+            {
+                "+" => Operation.Addition,
+                "-" => Operation.Substraction,
+                "*" => Operation.Multiplication,
+                "/" => Operation.Division,
+                _ => Operation.None,
+            };
+            tbScreen.Text += $" {operation} ";
+
+            if (_isTheResultOnTheScreen)
+            {
+                _isTheResultOnTheScreen = false;
+                tbScreen.Text = string.Empty;
+            }
+
+            SetOperationBtnState(false);
+            SetResultBtnState(false);
         }
 
         private void OnBtnResultClick(object sender, EventArgs e)
         {
+            if (_currentOperation == Operation.None)
+                return;
 
+            var firstNumber = double.Parse(_firstValue);
+            var secondNumber = double.Parse(_secondValue);
+            double result = Calculate(firstNumber, secondNumber);
+
+            tbScreen.Text = result.ToString();
+            _secondValue = string.Empty;
+            _currentOperation = Operation.None;
+            _isTheResultOnTheScreen = true;
+            SetResultBtnState(true);
+            SetBtnCommaState(true);
+
+        }
+        private double Calculate(double firstNumber, double secondNumber)
+        {
+            switch (_currentOperation)
+            {
+                case Operation.None:
+                    return firstNumber;
+                case Operation.Addition:
+                    return firstNumber + secondNumber;
+                case Operation.Substraction:
+                    return firstNumber - secondNumber;
+                case Operation.Multiplication:
+                    return firstNumber * secondNumber;
+                case Operation.Division:
+                    return firstNumber / secondNumber;
+            }
+            return 0;
         }
 
         private void OnBtnClearClick(object sender, EventArgs e)
         {
-
+            tbScreen.Text = string.Empty;
+            _firstValue = string.Empty;
+            _secondValue = string.Empty;
+            _currentOperation = Operation.None;
+        }
+        private void SetOperationBtnState(bool value)
+        {
+            BtnAddition.Enabled = value;
+            BtnSubstraction.Enabled = value;
+            BtnMultiplication.Enabled = value;
+            BtnDivision.Enabled = value;
+        }
+        private void SetResultBtnState(bool value)
+        {
+            BtnResult.Enabled = value;
+        }
+        private void SetBtnCommaState(bool value)
+        {
+            BtnComma.Enabled = value;
         }
     }
 }
