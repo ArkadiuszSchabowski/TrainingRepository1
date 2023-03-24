d48d47b887b4b71cb3e1a2c12e76e72002fed9e3
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-25 00:58:23 +0100

    Calculator will be write in the future.

--- TestKalkulator/Form1.cs
+++ TestKalkulator/Form1.cs
@@ -2,7 +2,6 @@ namespace TestKalkulator
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
@@ -12,16 +11,14 @@ namespace TestKalkulator
         {
         }
 
-
         private void OnBtnOperationClick(object sender, EventArgs e)
+        {
+        }
+        private void OnBtnResultClick(object sender, EventArgs e)
         {
         }
 
         private void OnBtnClearClick(object sender, EventArgs e)
-        {
-        }
-
-        private void OnBtnResultClick(object sender, EventArgs e)
         {
         }
     }
