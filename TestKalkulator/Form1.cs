71d77a41a0f2d08f3816b4eff1567722f56bb1b8
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-24 02:07:23 +0100

    Pig Calculator training

--- TestKalkulator/Form1.cs
+++ TestKalkulator/Form1.cs
@@ -21,71 +21,19 @@ namespace TestKalkulator
 
         private void OnBtnNumberClick(object sender, EventArgs e)
         {
-
-            var clickedValue = (sender as Button).Text;
-            tbScreen.Text += clickedValue;
         }
 
         private void OnBtnOperationClick(object sender, EventArgs e)
         {
-            tbScreen.Text = _firstValue;
-            tbScreen.Text = string.Empty;
-
-            var operation = (sender as Button).Text;
-
-            tbScreen.Text += operation;
-
-            tbScreen.Text = _secondValue;
-
-            double firstNumber = double.Parse(_firstValue);
-            double secondNumber = double.Parse(_firstValue);
-
-            double result = Calculate(firstNumber, secondNumber);
-
-            BtnAddition.Enabled = false;
-            BtnSubstraction.Enabled = false;
-            BtnMultiplication.Enabled = false;
-            BtnDivision.Enabled = false;
         }
 
         private void OnBtnClearClick(object sender, EventArgs e)
         {
-            tbScreen.Text = string.Empty;
-
-            BtnAddition.Enabled = true;
-            BtnSubstraction.Enabled = true;
-            BtnMultiplication.Enabled = true;
-            BtnDivision.Enabled = true;
         }
 
         private void OnBtnResultClick(object sender, EventArgs e)
         {
 
-        }
-        private double Calculate(double firstNumber, double secondNumber)
-        {
-
-            switch (_operation)
-            {
-                case Operation.None:
-                    return firstNumber;
-                case Operation.Addition:
-                    return firstNumber + secondNumber;
-                case Operation.Substraction:
-                    return firstNumber - secondNumber;
-                case Operation.Multiplication:
-                    return firstNumber * secondNumber;
-                case Operation.Division:
-                    return firstNumber / secondNumber;
-            }
-            return 0;
-        }
-
-        private void SetBtnResultState()
-        {
-        }
-        private void SetBtnOperationState()
-        {
         }
     }
 
