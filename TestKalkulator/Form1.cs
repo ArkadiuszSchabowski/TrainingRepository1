da5a393e51201e241edfa299c2bf9362bc4afd0c
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-24 02:07:58 +0100

    training

--- TestKalkulator/Form1.cs
+++ TestKalkulator/Form1.cs
@@ -1,18 +1,7 @@
 namespace TestKalkulator
 {
-    public enum Operation
-    {
-        None,
-        Addition,
-        Substraction,
-        Multiplication,
-        Division
-    }
     public partial class Form1 : Form
-    {
-        private string _firstValue = "0";
-        private string _secondValue = "0";
-        private Operation _operation;
+    { 
 
         public Form1()
         {
