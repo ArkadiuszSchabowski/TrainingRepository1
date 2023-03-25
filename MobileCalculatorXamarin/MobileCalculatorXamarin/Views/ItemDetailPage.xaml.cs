53adeef4f04074a60436bcbb271f3f9059da5aa9
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-25 18:16:10 +0100

    CalculatorASP.NETCORE Project

---
+++ MobileCalculatorXamarin/MobileCalculatorXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobileCalculatorXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobileCalculatorXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
