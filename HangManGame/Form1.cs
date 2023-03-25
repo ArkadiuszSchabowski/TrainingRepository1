5f784756fa084db7fdf392f0b75de77ab40a14c4
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-25 01:12:03 +0100

    HangManGame

---
+++ HangManGame/Form1.cs
@@ -0,0 +1,11 @@
+namespace HangManGame
+{
+    //U¿ytkownik próboje odgadn¹æ has³o, przechowane w pliku. Za kazda proba moze podac dodatkowa litere pomocnicza, ktora wyswietla sie w hasle. Has³o jest losowane spoœród wielu. Jesli mu sie uda wyswietla sie komunikat i generowane jest nowe haslo haslo, jesli nie zaczyna sie rysowac wisielec. Po 7 nieudanej probie odgadniecia hasla gracz przegrywa. 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
