b6af670eb43ca8c989dd0b9a082c8def5bd4e643
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-26 17:05:50 +0200

    HangMan Game - Graphic Interface done

--- HangManGame/Form1.Designer.cs
+++ HangManGame/Form1.Designer.cs
@@ -28,12 +28,435 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            button21 = new Button();
+            button22 = new Button();
+            button23 = new Button();
+            button24 = new Button();
+            button25 = new Button();
+            button26 = new Button();
+            button27 = new Button();
+            button28 = new Button();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            Tekst = new Label();
+            button29 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(12, 179);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Q";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(659, 255);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 70);
+            button2.TabIndex = 1;
+            button2.Text = "Enter";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(12, 293);
+            button3.Name = "button3";
+            button3.Size = new Size(641, 32);
+            button3.TabIndex = 2;
+            button3.Text = "Spacja";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.ForeColor = Color.Black;
+            button4.Location = new Point(567, 255);
+            button4.Name = "button4";
+            button4.Size = new Size(86, 32);
+            button4.TabIndex = 3;
+            button4.Text = "M";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.ForeColor = Color.Black;
+            button5.Location = new Point(475, 255);
+            button5.Name = "button5";
+            button5.Size = new Size(86, 32);
+            button5.TabIndex = 4;
+            button5.Text = "N";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.ForeColor = Color.Black;
+            button6.Location = new Point(384, 255);
+            button6.Name = "button6";
+            button6.Size = new Size(85, 32);
+            button6.TabIndex = 5;
+            button6.Text = "B";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.ForeColor = Color.Black;
+            button7.Location = new Point(291, 255);
+            button7.Name = "button7";
+            button7.Size = new Size(87, 32);
+            button7.TabIndex = 6;
+            button7.Text = "V";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.ForeColor = Color.Black;
+            button8.Location = new Point(195, 255);
+            button8.Name = "button8";
+            button8.Size = new Size(90, 32);
+            button8.TabIndex = 7;
+            button8.Text = "C";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.ForeColor = Color.Black;
+            button9.Location = new Point(99, 255);
+            button9.Name = "button9";
+            button9.Size = new Size(90, 32);
+            button9.TabIndex = 8;
+            button9.Text = "X";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.ForeColor = Color.Black;
+            button10.Location = new Point(12, 255);
+            button10.Name = "button10";
+            button10.Size = new Size(81, 32);
+            button10.TabIndex = 9;
+            button10.Text = "Z";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.ForeColor = Color.Black;
+            button11.Location = new Point(683, 217);
+            button11.Name = "button11";
+            button11.Size = new Size(85, 32);
+            button11.TabIndex = 10;
+            button11.Text = "L";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.ForeColor = Color.Black;
+            button12.Location = new Point(595, 217);
+            button12.Name = "button12";
+            button12.Size = new Size(82, 32);
+            button12.TabIndex = 11;
+            button12.Text = "K";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.ForeColor = Color.Black;
+            button13.Location = new Point(517, 217);
+            button13.Name = "button13";
+            button13.Size = new Size(72, 32);
+            button13.TabIndex = 12;
+            button13.Text = "J";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.ForeColor = Color.Black;
+            button14.Location = new Point(439, 217);
+            button14.Name = "button14";
+            button14.Size = new Size(72, 32);
+            button14.TabIndex = 13;
+            button14.Text = "H";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.ForeColor = Color.Black;
+            button15.Location = new Point(348, 217);
+            button15.Name = "button15";
+            button15.Size = new Size(85, 32);
+            button15.TabIndex = 14;
+            button15.Text = "G";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.ForeColor = Color.Black;
+            button16.Location = new Point(260, 217);
+            button16.Name = "button16";
+            button16.Size = new Size(82, 32);
+            button16.TabIndex = 15;
+            button16.Text = "F";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.ForeColor = Color.Black;
+            button17.Location = new Point(182, 217);
+            button17.Name = "button17";
+            button17.Size = new Size(72, 32);
+            button17.TabIndex = 16;
+            button17.Text = "D";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.ForeColor = Color.Black;
+            button18.Location = new Point(104, 217);
+            button18.Name = "button18";
+            button18.Size = new Size(72, 32);
+            button18.TabIndex = 17;
+            button18.Text = "S";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.ForeColor = Color.Black;
+            button19.Location = new Point(12, 217);
+            button19.Name = "button19";
+            button19.Size = new Size(86, 32);
+            button19.TabIndex = 18;
+            button19.Text = "A";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.ForeColor = Color.Black;
+            button20.Location = new Point(696, 179);
+            button20.Name = "button20";
+            button20.Size = new Size(72, 32);
+            button20.TabIndex = 19;
+            button20.Text = "P";
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            button21.ForeColor = Color.Black;
+            button21.Location = new Point(618, 179);
+            button21.Name = "button21";
+            button21.Size = new Size(72, 32);
+            button21.TabIndex = 20;
+            button21.Text = "O";
+            button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            button22.ForeColor = Color.Black;
+            button22.Location = new Point(543, 179);
+            button22.Name = "button22";
+            button22.Size = new Size(72, 32);
+            button22.TabIndex = 21;
+            button22.Text = "I";
+            button22.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            button23.ForeColor = Color.Black;
+            button23.Location = new Point(480, 179);
+            button23.Name = "button23";
+            button23.Size = new Size(59, 32);
+            button23.TabIndex = 22;
+            button23.Text = "U";
+            button23.UseVisualStyleBackColor = true;
+            // 
+            // button24
+            // 
+            button24.ForeColor = Color.Black;
+            button24.Location = new Point(402, 179);
+            button24.Name = "button24";
+            button24.Size = new Size(72, 32);
+            button24.TabIndex = 23;
+            button24.Text = "Y";
+            button24.UseVisualStyleBackColor = true;
+            // 
+            // button25
+            // 
+            button25.ForeColor = Color.Black;
+            button25.Location = new Point(324, 179);
+            button25.Name = "button25";
+            button25.Size = new Size(72, 32);
+            button25.TabIndex = 24;
+            button25.Text = "T";
+            button25.UseVisualStyleBackColor = true;
+            // 
+            // button26
+            // 
+            button26.ForeColor = Color.Black;
+            button26.Location = new Point(246, 179);
+            button26.Name = "button26";
+            button26.Size = new Size(72, 32);
+            button26.TabIndex = 25;
+            button26.Text = "R";
+            button26.UseVisualStyleBackColor = true;
+            // 
+            // button27
+            // 
+            button27.ForeColor = Color.Black;
+            button27.Location = new Point(168, 179);
+            button27.Name = "button27";
+            button27.Size = new Size(72, 32);
+            button27.TabIndex = 26;
+            button27.Text = "E";
+            button27.UseVisualStyleBackColor = true;
+            // 
+            // button28
+            // 
+            button28.ForeColor = Color.Black;
+            button28.Location = new Point(90, 179);
+            button28.Name = "button28";
+            button28.Size = new Size(72, 32);
+            button28.TabIndex = 27;
+            button28.Text = "W";
+            button28.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(541, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(217, 151);
+            pictureBox1.TabIndex = 28;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(499, 23);
+            textBox1.TabIndex = 29;
+            // 
+            // Tekst
+            // 
+            Tekst.AutoSize = true;
+            Tekst.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Tekst.ForeColor = Color.Black;
+            Tekst.Location = new Point(22, 72);
+            Tekst.Name = "Tekst";
+            Tekst.Size = new Size(160, 37);
+            Tekst.TabIndex = 30;
+            Tekst.Text = "Podaj hasło:";
+            // 
+            // button29
+            // 
+            button29.Location = new Point(6, 8);
+            button29.Name = "button29";
+            button29.Size = new Size(129, 43);
+            button29.TabIndex = 31;
+            button29.Text = "Nowa Gra";
+            button29.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LimeGreen;
+            ClientSize = new Size(963, 328);
+            Controls.Add(button29);
+            Controls.Add(Tekst);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button28);
+            Controls.Add(button27);
+            Controls.Add(button26);
+            Controls.Add(button25);
+            Controls.Add(button24);
+            Controls.Add(button23);
+            Controls.Add(button22);
+            Controls.Add(button21);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = Color.Black;
+            Name = "Form1";
+            Text = "Wisielec";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private Button button21;
+        private Button button22;
+        private Button button23;
+        private Button button24;
+        private Button button25;
+        private Button button26;
+        private Button button27;
+        private Button button28;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Label Tekst;
+        private Button button29;
     }
 }
--- TestKalkulator/Form1.cs
+++ TestKalkulator/Form1.cs
@@ -1,25 +1,154 @@
 namespace TestKalkulator
 {
+    public enum Operation
+    {
+        None,
+        Addition,
+        Substraction,
+        Multiplication,
+        Division,
+    }
     public partial class Form1 : Form
     {
+
+        private string _firstValue;
+        private string _secondValue;
+        private Operation _currentOperation = Operation.None;
+        private bool _isResultOnTheScreen = false;
+
         public Form1()
         {
             InitializeComponent();
+            tbScreen.Text = "0";
         }
 
         private void OnBtnNumberClick(object sender, EventArgs e)
         {
+            var clickedValue = (sender as Button).Text;
+
+            if(_isResultOnTheScreen != true)
+            {
+                SetBtnNumbersState(true);
+            }
+
+            if (tbScreen.Text == "0")
+            {
+                tbScreen.Text = string.Empty;
+            }
+            tbScreen.Text += clickedValue;
+
+            if (_currentOperation != Operation.None)
+            {
+                _secondValue += clickedValue;
+            }
+            if (_currentOperation != Operation.None)
+            {
+                SetBtnOperationState(true);
+            }
         }
 
         private void OnBtnOperationClick(object sender, EventArgs e)
         {
+            _firstValue = tbScreen.Text;
+
+            var operation = (sender as Button).Text;
+            tbScreen.Text += $" {operation} ";
+
+            _currentOperation = operation switch
+            {
+                "+" => Operation.Addition,
+                "-" => Operation.Substraction,
+                "*" => Operation.Multiplication,
+                "/" => Operation.Division,
+                _ => Operation.None
+            };
+            if (_currentOperation != Operation.None)
+            {
+                SetBtnOperationState(false);
+            }
+
         }
         private void OnBtnResultClick(object sender, EventArgs e)
         {
+
+            double firstNumber = double.Parse(_firstValue);
+            double secondNumber = double.Parse(_secondValue);
+
+            double result = Calculate(firstNumber, secondNumber);
+
+            tbScreen.Text = result.ToString();
+
+            _isResultOnTheScreen = true;
+            _secondValue = string.Empty;
+            SetBtnOperationState(true);
+
+            if (_isResultOnTheScreen)
+            {
+                SetBtnNumbersState(false);
+                _isResultOnTheScreen = false;
+            }
+            if(_currentOperation != Operation.None)
+            {
+                SetBtnNumbersState(true);
+            }
+            _currentOperation = Operation.None;
+        }
+        public double Calculate(double firstNumber, double secondNumber)
+        {
+            switch (_currentOperation)
+            {
+                case Operation.None:
+                    return firstNumber;
+                case Operation.Addition:
+                    return firstNumber + secondNumber;
+                case Operation.Substraction:
+                    return firstNumber - secondNumber;
+                case Operation.Multiplication:
+                    return firstNumber * secondNumber;
+                case Operation.Division:
+                    if(secondNumber == 0)
+                    {
+                        MessageBox.Show("Nie mo¿na dzieliæ przez zero");
+                    }
+                    return firstNumber / secondNumber;
+            }
+            return 0;
         }
 
         private void OnBtnClearClick(object sender, EventArgs e)
         {
+            tbScreen.Text = "0";
+            _firstValue = string.Empty;
+            _secondValue = string.Empty;
+            _currentOperation = Operation.None;
+            SetBtnOperationState(true);
+            SetBtnNumbersState(true);
+        }
+        private void SetBtnOperationState(bool value)
+        {
+            BtnAddition.Enabled = value;
+            BtnSubstraction.Enabled = value;
+            BtnMultiplication.Enabled = value;
+            BtnDivision.Enabled = value;
+        }
+        private void SetBtnResultState(bool value)
+        {
+            BtnResult.Enabled = value;
+        }
+        private void SetBtnNumbersState(bool value)
+        {
+            Btn1.Enabled = value;
+            Btn2.Enabled = value;
+            Btn3.Enabled = value;
+            Btn4.Enabled = value;
+            Btn4.Enabled = value;
+            Btn5.Enabled = value;
+            Btn6.Enabled = value;
+            Btn7.Enabled = value;
+            Btn8.Enabled = value;
+            Btn9.Enabled = value;
+            Btn0.Enabled = value;
+            BtnComma.Enabled = value;
         }
     }
 }
