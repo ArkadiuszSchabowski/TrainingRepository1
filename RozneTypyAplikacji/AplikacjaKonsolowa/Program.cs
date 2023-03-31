ca8e3e532583c570099fdedc20961f97623b8889
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-31 22:13:31 +0200

    Komentarz dokumentacyjny

---
+++ RozneTypyAplikacji/AplikacjaKonsolowa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaKonsolowa
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
