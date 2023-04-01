984261c893842a6231f016fd871475b0a55cae17
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-01 21:58:03 +0200

    Operator trójargumentowy

--- checkUserAge/Program.cs
+++ checkUserAge/Program.cs
@@ -10,6 +10,12 @@ namespace checkUserAge
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj wiek");
+            int wiek = int.Parse(Console.ReadLine());
+            string wypiszWiek = wiek > 20 ? "masz więcej niż 20 lat" : "Masz 20 lub mniej lat";
+            Console.WriteLine(wypiszWiek);
+            Console.ReadKey();
+            
         }
     }
 }
