5f566d92a6ff3c48f4b4f4ae4f7b507b12ca83c8
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-02 19:16:00 +0200

    Publicate Desktop_Calculator

--- AprilCourse/Program.cs
+++ AprilCourse/Program.cs
@@ -1,27 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AprilCourse
+class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine($"byte: { byte.MinValue}, { byte.MaxValue}");
-            Console.WriteLine($"short: {short.MinValue}, {short.MaxValue}");
-            Console.WriteLine($"int: {int.MinValue}, {int.MaxValue}");
-            Console.WriteLine($"long: {long.MinValue}, {long.MaxValue}");
-            Console.WriteLine();
-            Console.WriteLine($"float: {float.MinValue}, {float.MaxValue}");
-            Console.WriteLine($"double: {double.MinValue}, {double.MaxValue}");
-            Console.WriteLine($"decimal: {decimal.MinValue}, {decimal.MaxValue}");
-            Console.WriteLine();
-            Console.ReadKey();
-        }
-    }
+    static void Main(string[] args)
+    { }
 }
