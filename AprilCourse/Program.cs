314b8f4a38ac1190b73def12b67fd7423669b140
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-04 20:47:44 +0200

    Losowa liczba

--- AprilCourse/Program.cs
+++ AprilCourse/Program.cs
@@ -4,20 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] array2D = new int[2, 3];
-
-        array2D[0, 0] = 4;
-        array2D[0, 1] = 7;
-        array2D[0, 2] = 6;
-        array2D[1, 0] = 9;
-        array2D[1, 1] = 2;
-        array2D[1, 2] = 3;
-
-        foreach (var item in array2D)
+        try
         {
-            Console.WriteLine(item);
-        }
+            Console.WriteLine("Komputer wylosował liczbę z przedziału 1-10.\nSpróbuj ją odgadnąć");
+            Random rnd = new Random();
+            int computerNumber = rnd.Next(1, 11);
+            int userNumber = 0;
+            int razy = 0;
 
-        Console.ReadKey();
+            do
+            {
+                bool check = int.TryParse(Console.ReadLine(), out userNumber);
+                Console.WriteLine();
+
+                if (!check || userNumber < 1 || userNumber > 10)
+                {
+                    Console.WriteLine("Nieprawidłowa wartość");
+                }
+                else
+                {
+                    razy++;
+                    if (userNumber < computerNumber)
+                    {
+                        Console.WriteLine("Twoja liczba jest mniejsza od wylosowanej");
+                    }
+                    else if (userNumber > computerNumber)
+                    {
+                        Console.WriteLine("Twoja liczba jest większa od wylosowanej");
+                    }
+                }
+            } while (userNumber != computerNumber);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Brawo udało się odgadnąć liczbę za {razy} razem.");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.ReadKey();
+        }
     }
 }
