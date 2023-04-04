6265dd07cbedaf642491572fb149eaa99b76b2b3
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-04 23:12:15 +0200

    Palindromy

--- AprilCourse/Program.cs
+++ AprilCourse/Program.cs
@@ -4,53 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Title = "Zgadnij liczbę";
-        try
-        {
-            Console.WriteLine("Komputer wylosował liczbę z przedziału 1-10.\nSpróbuj ją odgadnąć");
-            Random rnd = new Random();
-            int computerNumber = rnd.Next(1, 11);
-            int userNumber = 0;
-            int razy = 0;
-            bool[] array = new bool[10];
+        Console.WriteLine("Zgadnij czy dane słowo jest palindromem");
+        string word = Console.ReadLine().ToUpper().Replace(" ", "");
+        bool isTrue = true;
 
-            do
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] != word[word.Length - i - 1])
             {
-                bool check = int.TryParse(Console.ReadLine(), out userNumber);
-                Console.WriteLine();
-
-                if (!check || userNumber < 1 || userNumber > 10)
-                {
-                    Console.WriteLine("Nieprawidłowa wartość");
-                }
-
-                else if (array[userNumber -1])
-                {
-                    Console.WriteLine("Podano tą samą liczbę");
-                }
-
-                else
-                {
-                    array[userNumber -1] = true;
-                    razy++;
-                    if (userNumber < computerNumber)
-                    {
-                        Console.WriteLine("Twoja liczba jest mniejsza od wylosowanej");
-                    }
-                    else if (userNumber > computerNumber)
-                    {
-                        Console.WriteLine("Twoja liczba jest większa od wylosowanej");
-                    }
-                }
-            } while (userNumber != computerNumber);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Brawo udało się odgadnąć liczbę za {razy} razem.");
-            Console.ReadKey();
+                isTrue = false;
+                break;
+            }
         }
-        catch (Exception ex)
+
+        if (isTrue)
         {
-            Console.WriteLine(ex.Message);
-            Console.ReadKey();
+            Console.WriteLine("Słowo jest palindromem");
         }
+        else
+        {
+            Console.WriteLine("Słowo nie jest palindromem");
+        }
+        Console.ReadKey();
     }
 }
