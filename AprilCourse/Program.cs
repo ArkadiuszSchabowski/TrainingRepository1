b80faabc062c7682490847e0111a428b26103f43
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-05 11:11:41 +0200

    Funkcje bezargumentowe typu string

--- AprilCourse/Program.cs
+++ AprilCourse/Program.cs
@@ -4,27 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Zgadnij czy dane słowo jest palindromem");
-        string word = Console.ReadLine().ToUpper().Replace(" ", "");
-        bool isTrue = true;
-
-        for (int i = 0; i < word.Length; i++)
-        {
-            if (word[i] != word[word.Length - i - 1])
-            {
-                isTrue = false;
-                break;
-            }
-        }
-
-        if (isTrue)
-        {
-            Console.WriteLine("Słowo jest palindromem");
-        }
-        else
-        {
-            Console.WriteLine("Słowo nie jest palindromem");
-        }
+        Console.WriteLine("Podaj swoje imię");
+        string name = Console.ReadLine();
+        Hello(name);
         Console.ReadKey();
+    }
+    static void Hello(string name)
+    {
+        Console.WriteLine($"Witaj {name}!");
     }
 }
