3bca946aa1aeea6724f39d7ea4e73fa8f2b2c635
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-06 13:07:02 +0200

    Params zrozumiane

--- AprilCourse/Program.cs
+++ AprilCourse/Program.cs
@@ -4,15 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("{0} {1} {2}", "Hello", "World", "!!!");
-        Print("{0} {1} {2}", "Hello", "World", "!!!");
+        Params("{0} {1} {2}", "Hello", "World", "!!!");
+
         Console.ReadKey();
     }
-    static void Print (string text, params string[] arg)
+
+    static void Params(string text, params string[] arg)
     {
         for (int i = 0; i < arg.Length; i++)
         {
-            text = text.Replace("{"+ i +"}", arg[i]);
+            text = text.Replace("{" +i + "}", arg[i]);
         }
         Console.WriteLine(text);
     }
