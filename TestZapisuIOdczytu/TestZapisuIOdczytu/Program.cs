5ee8be7ddaad99e0f1daed89a4316339bc40a0f4
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-09 12:13:08 +0200

    StreamWriter sk = new StreamWriter(path, true);

---
+++ TestZapisuIOdczytu/TestZapisuIOdczytu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestZapisuIOdczytu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"../../../zapis2.txt";
+
+            StreamWriter sw = new StreamWriter("../../../zapis.txt");
+            Console.WriteLine("Podaj słowo do zapisu");
+            sw.WriteLine(Console.ReadLine());
+            sw.Close();
+
+            StreamWriter sk = new StreamWriter(path, true);
+            Console.WriteLine("Podaj słowo do zapisu");
+            sk.WriteLine(Console.ReadLine());
+            sk.Close();
+        }
+    }
+}
