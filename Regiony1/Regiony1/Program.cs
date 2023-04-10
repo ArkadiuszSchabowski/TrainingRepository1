2575fda1d9108d455b01a23bdd901e1787d10bca
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-10 22:01:38 +0200

    End day

--- Regiony1/Regiony1/Program.cs
+++ Regiony1/Regiony1/Program.cs
@@ -3,19 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Regiony1
 {
-    #region MyRegionClass
-
     internal class Program
     {
-        static void Main(string[] args) =>
-        #region MyRegion
-                        Console.WriteLine("Tu będzie region");
-        #endregion
+        static void Main(string[] args)
+        {
+            string path = $"../../../Test.txt";
 
-    #endregion
 
+            #region Zapis1
+
+            //StreamWriter sw = new StreamWriter(path, true);
+
+            //Console.WriteLine("Podaj tekst do przechowania");
+            //sw.WriteLine(Console.ReadLine());
+            //sw.Close();
+            #endregion
+
+            #region Odczyt1
+
+            //Console.WriteLine();
+            //Console.WriteLine(File.ReadAllText(path));
+            #endregion
+
+            #region Odczyt2
+
+            StreamReader sr = new StreamReader(path);
+
+            string line;
+
+            List<string> lista= new List<string>();
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                lista.Add(line);
+            }
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item + " Element w liście");
+            }
+            sr.Close();
+            Console.ReadKey();
+            #endregion
+        }
     }
 }
