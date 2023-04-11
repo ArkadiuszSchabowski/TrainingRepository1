5f0781efbd5da7374846474d78652dbc3215a65c
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-11 15:11:36 +0200

    Mini test

---
+++ KlasyIObiekty/KlasyIObiekty/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasyIObiekty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Meble.MebleDrewniane();
+
+            Console.WriteLine();
+
+            Szyba szyba = new Szyba();
+            szyba.Szklo();
+            Console.ReadKey();
+        }
+    }
+    public class Szyba
+    {
+        public void Szklo()
+        {
+            Console.WriteLine("Jestem wykonana ze szkła");
+        }
+    }
+
+    public static class Meble
+    {
+        public static void MebleDrewniane()
+        {
+            Console.WriteLine("Jestem wykonany z drzewa");
+        }
+    }
+}
