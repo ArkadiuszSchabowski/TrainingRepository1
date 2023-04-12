b6cb4bd5652fcb96c44a2e02ed0a1b82db70d665
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-12 16:00:49 +0200

    English language in Visual Studio, Firefox and System. Class and properties training.

--- KlasyIObiekty/KlasyIObiekty/Program.cs
+++ KlasyIObiekty/KlasyIObiekty/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,25 +11,56 @@ namespace KlasyIObiekty
     {
         static void Main(string[] args)
         {
-            Person person = new Person("Arek", 31);
-            person.IntroduceYourself();
-            Person person2 = new Person("Ania", 24);
-            person2.IntroduceYourself();
+            Person person = new Person();
+            person.SetName("Dominika");
+            person.SetName("-");
+            Console.WriteLine(person.GetName());
+            person.setWiek(19);
+            person.setWiek(101);
+            Console.WriteLine(person.getWiek());
             Console.ReadKey();
         }
     }
     public class Person
     {
-        public Person(string name, int age)
+
+        //public Person(string name, int age)
+        //{
+        //    //_name = name;
+        //    //_age = age;
+        //}
+
+        private string _name;
+        private int _age;
+
+
+        public string GetName()
         {
-            _name = name;
-            _age = age;
+            return _name;
         }
-        public string _name;
-        public int _age;
+        public void SetName(string value)
+        {
+            if (value.Length > 3)
+            {
+                _name = value;
+            }
+        }
+
         public void IntroduceYourself()
         {
-            Console.WriteLine($"I'm {_name} and I am {_age} years old");
+            Console.WriteLine($"I'm {_name} and I am {_age} years old.");
+        }
+        public int getWiek()
+        {
+            return _age;
+        }
+        public void setWiek(int value)
+        {
+            if (value > 0 && value < 100)
+            {
+                _age = value;
+            }
+
         }
     }
 }
