d0a033e4fcd3d3b77c66b58faa7421f03b2eff66
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-15 16:00:20 +0200

    Hard day

---
+++ GameMemory/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameMemory
+{
+
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        List<int> list = new List<int>
+    {
+        1, 1, 2, 2, 3, 3, 4, 4,
+        5, 5, 6, 6, 7, 7, 8, 8
+    };
+        public Form1()
+        {
+            InitializeComponent();
+            AssignItemsToSquares();
+        }
+        public void AssignItemsToSquares()
+        {
+            Label label;
+
+            int position = rnd.Next(0, 16);
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!list.Contains(list[position]))
+                {
+                    list[i] = position;
+                }
+                tableLayoutPanel1.Controls = position;
+            }
+        }
+    } 
+}
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -13,25 +13,73 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         Random random = new Random();
-        List<string> icons = new List<string>()
+        List<string> lista = new List<string>()
         {
             "a", "a", "b", "b", "c", "c","d", "d",
             "e", "e", "f", "f", "g", "g","h", "h"
         };
 
-        Label firstClicked, secondClicked;
-
-        
+        Label firstClicked, secondClicked;  
 
         public Form1()
         {
             InitializeComponent();
             AssignIconsToSquares();
         }
+
+        private void label_click(object sender, EventArgs e)
+        {
+            if (firstClicked != null & secondClicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                clickedLabel.ForeColor = Color.Black;
+                return;
+            }
+
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+            }
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+            }
+            else
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = Color.Orange;
+            secondClicked.ForeColor = Color.Orange;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
         private void AssignIconsToSquares()
         {
             Label label;
-            int randomNumber;
+            int position;
 
             for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
             {
@@ -42,10 +90,10 @@ namespace WindowsFormsApp1
                 else
                     continue;
 
-                randomNumber = random.Next(0,icons.Count);
-                label.Text = icons[randomNumber];
+                position = random.Next(0, lista.Count);
+                label.Text = lista[position];
 
-                icons.RemoveAt(randomNumber);
+                lista.RemoveAt(position);
             }
         }
     }
