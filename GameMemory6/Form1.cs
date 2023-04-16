29fcaf935f9554cd9379f575c2bcdffe09460a8f
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-04-16 11:16:47 +0200

    MemoryGame done

--- GameMemory6/Form1.cs
+++ GameMemory6/Form1.cs
@@ -8,18 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace GameMemory6
 {
     public partial class Form1 : Form
     {
-        Random random = new Random();
-        List<string> lista = new List<string>()
+
+        Random rnd = new Random();
+        List<string> list = new List<string>()
         {
-            "a", "a", "%", "%", "c", "c","d", "d",
-            "e", "e", "f", "f", "g", "g","h", "h"
+            "a", "a", "%", "%", "c", "c", "d", "d",
+            "e", "e", "f", "f", "g", "g", "h", "h"
         };
 
-        Label firstClicked, secondClicked;  
+        Label firstClicked, secondClicked;
 
         public Form1()
         {
@@ -29,53 +30,62 @@ namespace WindowsFormsApp1
 
         private void label_click(object sender, EventArgs e)
         {
-            if (firstClicked != null & secondClicked != null)
-                return;
+            Label clicledLabel = sender as Label;
 
-            Label clickedLabel = sender as Label;
 
-            if (clickedLabel == null)
-                return;
-
-            if (clickedLabel.ForeColor == Color.Black)
-                return;
-
-            if (firstClicked == null)
+            if (firstClicked != null && secondClicked != null)
             {
-                firstClicked = clickedLabel;
-                clickedLabel.ForeColor = Color.Black;
                 return;
             }
 
+            if (clicledLabel == null)
+            {
+                return;
+            }
+            if (clicledLabel.ForeColor == Color.Black)
+            {
+                return;
+            }
             if (firstClicked == null)
             {
-                firstClicked = clickedLabel;
-                secondClicked.ForeColor = Color.Black;
-
+                firstClicked = clicledLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
             }
-            secondClicked = clickedLabel;
+
+            secondClicked = clicledLabel;
             secondClicked.ForeColor = Color.Black;
 
             if (firstClicked.Text == secondClicked.Text)
             {
                 firstClicked = null;
                 secondClicked = null;
+
+                CheckForWinner();
             }
             else
             timer1.Start();
-
         }
         private void CheckForWinner()
         {
+            Label label;
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
 
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+            MessageBox.Show("Congratulations!");
+            Close();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Stop();
 
-            firstClicked.ForeColor = Color.Orange;
-            secondClicked.ForeColor = Color.Orange;
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
 
             firstClicked = null;
             secondClicked = null;
@@ -91,14 +101,13 @@ namespace WindowsFormsApp1
                 if (tableLayoutPanel1.Controls[i] is Label)
                 {
                     label = (Label)tableLayoutPanel1.Controls[i];
+
+                    position = rnd.Next(0, list.Count);
+                    label.Text = list[position];
+
+                    list.RemoveAt(position);
                 }
-                else
-                    continue;
 
-                position = random.Next(0, lista.Count);
-                label.Text = lista[position];
-
-                lista.RemoveAt(position);
             }
         }
     }
