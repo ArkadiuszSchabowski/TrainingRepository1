959afb2d3a0977ededab4d1b8234eb86aff37325
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-05-01 12:33:37 +0200

    quiz Game

---
+++ quizGameWindowsForm/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace quizGameWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::quizGameWindowsForm.Properties.Resources.rozetka;
+            this.pictureBox1.Location = new System.Drawing.Point(122, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 212);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(92, 310);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(253, 32);
+            this.btnA.TabIndex = 1;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(93, 350);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(253, 32);
+            this.btnC.TabIndex = 2;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(351, 312);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(253, 32);
+            this.btnB.TabIndex = 3;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(352, 350);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(253, 32);
+            this.btnD.TabIndex = 4;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 247);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(538, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(709, 394);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "QUIZ O SWINKACH MORSKICH";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ quizGameWindowsForm/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizGameWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            QuestionOne();
+        }
+
+        public void QuestionOne()
+        {
+            pictureBox1.Image = Properties.Resources.rozetka;
+            textBox1.Text = "Jakiej rasy jest swinka przedstawiona na obrazku?";
+            btnA.Text = " A - Skinny";
+            btnB.Text = " B - Peruwianka";
+            btnC.Text = " C - Rozetka";
+            btnD.Text = " D - Dlugowlosa";
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string Answer = (sender as Button).Text;
+
+            switch (Answer)
+            {
+                case "A":
+                    break;
+                case "B":
+                    break;
+                case "C":
+                    break;
+                case "D":
+                    break;
+            }
+        }
+    }
+}
