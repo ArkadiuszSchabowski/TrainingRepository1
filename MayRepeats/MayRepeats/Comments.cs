c588c77d6fb6f3b918b856ac0d2da4bc16422bfd
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-05-09 19:38:05 +0200

    May repeats

--- MayRepeats/MayRepeats/Comments.cs
+++ MayRepeats/MayRepeats/Comments.cs
@@ -15,14 +15,15 @@ namespace MayRepeats
         * 4. Milionerzy z podzialem na klasy
         * 5. Odgadnij liczbe wylosowana przez komputer
         * 6. Papier, Kamien, Nozyce - DONE!
+        * 7. Gra Modulo - Hi, Hey, Hello piggy
         * 
         * WindowsFormApp:
         * 1.Kalkulator
-        * 2.MemoryGame
+        * 2.MemoryGame - DONE
         * 3.Quiz
         * 4.Kurczak lapiacy jajka
         * 5.Lista kontaktow
-        * 6.Lista Zakupow
+        * 6.Lista Zakupow - DONE
         */
     }
 }
