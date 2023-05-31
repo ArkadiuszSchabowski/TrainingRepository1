f1cee1850ab11b66d9fdde6c1db4d7c90076c0ca
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-05-31 10:29:35 +0200

    Sql Connector Project

--- SQLProject/SQLProject/Form1.cs
+++ SQLProject/SQLProject/Form1.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace SQLProject
 {
     public partial class Form1 : Form
     {
+        MySqlConnection sqlConn = new MySqlConnection();
+        MySqlCommand sqlCmd = new MySqlCommand();
+        DataTable dt = new DataTable();
+        String sqlQuery;
+        MySqlDataAdapter Dta = new MySqlDataAdapter();
+        MySqlDataReader sqlRd;
+
+        DataSet DS = new DataSet();
+
+        String server = "localhost";
+        String username = "root";
+        String password = "12345";
+        String database = "membership";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult iExit;
+
+            iExit = MessageBox.Show("Do you want close this aplication?","Information",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+            if (iExit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
