42accdf6cab90b9955beba3326bf41928ced63ae
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-06-03 21:35:08 +0200

    sql training with c#

---
+++ EnglishFlashCardProject/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace EnglishFlashCardProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.textPolish = new System.Windows.Forms.TextBox();
+            this.textEnglish = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textNumber = new System.Windows.Forms.TextBox();
+            this.LabelCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(68, 174);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(113, 37);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Dodaj Fiszke";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(460, 130);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(207, 174);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(113, 37);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Usun Fiszke";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(350, 174);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(113, 37);
+            this.btnModify.TabIndex = 3;
+            this.btnModify.Text = "Modyfikuj Fiszke";
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // textPolish
+            // 
+            this.textPolish.Location = new System.Drawing.Point(262, 51);
+            this.textPolish.Name = "textPolish";
+            this.textPolish.Size = new System.Drawing.Size(232, 20);
+            this.textPolish.TabIndex = 4;
+            // 
+            // textEnglish
+            // 
+            this.textEnglish.Location = new System.Drawing.Point(262, 86);
+            this.textEnglish.Name = "textEnglish";
+            this.textEnglish.Size = new System.Drawing.Size(232, 20);
+            this.textEnglish.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Znaczenie Polskie:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Znaczenie Angielskie:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Numer Fiszki:";
+            // 
+            // textNumber
+            // 
+            this.textNumber.Location = new System.Drawing.Point(262, 15);
+            this.textNumber.Name = "textNumber";
+            this.textNumber.Size = new System.Drawing.Size(232, 20);
+            this.textNumber.TabIndex = 9;
+            // 
+            // LabelCount
+            // 
+            this.LabelCount.AutoSize = true;
+            this.LabelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelCount.Location = new System.Drawing.Point(12, 133);
+            this.LabelCount.Name = "LabelCount";
+            this.LabelCount.Size = new System.Drawing.Size(265, 25);
+            this.LabelCount.TabIndex = 10;
+            this.LabelCount.Text = "Fiszek w bazie danych: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 362);
+            this.Controls.Add(this.LabelCount);
+            this.Controls.Add(this.textNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textEnglish);
+            this.Controls.Add(this.textPolish);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.TextBox textPolish;
+        private System.Windows.Forms.TextBox textEnglish;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textNumber;
+        private System.Windows.Forms.Label LabelCount;
+    }
+}
+
---
+++ EnglishFlashCardProject/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EnglishFlashCardProject
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection sqlConn = new MySqlConnection();
+        MySqlCommand sqlCmd = new MySqlCommand();
+        DataTable sqlDt = new DataTable();
+        String sqlQuery;
+        MySqlDataAdapter Dta = new MySqlDataAdapter();
+        MySqlDataReader sqlRd;
+
+        DataSet DS = new DataSet();
+
+        String server = "localhost";
+        String username = "root";
+        String password = "YES";
+        String database = "englishflashcards";
+        public Form1()
+        {
+            InitializeComponent();
+            UploadData();
+            CountFishes();
+        }
+        public void CountFishes()
+        {
+            LabelCount.Text = "Fiszek w bazie danych: " + (dataGridView1.RowCount -2).ToString();
+        }
+        public void UploadData()
+        {
+            sqlConn.ConnectionString = "server=" + server + ";"
+                  + "username =" + username + ";"
+                  + "password =" + password + ";"
+                  + "database =" + database;
+
+            sqlConn.Open();
+            sqlCmd.Connection = sqlConn;
+            sqlCmd.CommandText = "SELECT * FROM englishflashcards.englishflashcards ORDER BY NR DESC";
+            sqlRd = sqlCmd.ExecuteReader();
+            sqlDt.Load(sqlRd);
+            sqlRd.Close();
+            sqlConn.Close();
+
+            dataGridView1.DataSource = sqlDt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConn.ConnectionString = "server=" + server + ";" + "username =" + username + ";" + "password =" + password + ";" + "database =" + database;
+                sqlConn.Open();
+
+                sqlQuery = "INSERT INTO englishflashcards.englishflashcards (NR, PL, ENG) VALUES('" + (textNumber.Text +"','" + textPolish.Text + "','" + textEnglish.Text  + "')").ToUpper();
+
+                sqlCmd = new MySqlCommand(sqlQuery, sqlConn);
+                sqlRd = sqlCmd.ExecuteReader();
+                sqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConn.Close();
+            }
+            UploadData();
+            CountFishes();
+        }
+    }
+}
