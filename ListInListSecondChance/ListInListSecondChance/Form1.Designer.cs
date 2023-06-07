9037bbbe3b3c03e7000294f2c4a775123243afcc
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-06-07 20:30:16 +0200

    Work with MyCvProject

--- ListInListSecondChance/ListInListSecondChance/Form1.Designer.cs
+++ ListInListSecondChance/ListInListSecondChance/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.btnAddListCountry = new System.Windows.Forms.Button();
             this.btnAddContractor = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -41,7 +42,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(67, 39);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(276, 147);
+            this.listBox1.Size = new System.Drawing.Size(276, 238);
             this.listBox1.TabIndex = 0;
             // 
             // listBox2
@@ -54,7 +55,7 @@
             // 
             // btnDisplay
             // 
-            this.btnDisplay.Location = new System.Drawing.Point(67, 192);
+            this.btnDisplay.Location = new System.Drawing.Point(67, 283);
             this.btnDisplay.Name = "btnDisplay";
             this.btnDisplay.Size = new System.Drawing.Size(125, 33);
             this.btnDisplay.TabIndex = 2;
@@ -64,7 +65,7 @@
             // 
             // btnAddListCountry
             // 
-            this.btnAddListCountry.Location = new System.Drawing.Point(67, 308);
+            this.btnAddListCountry.Location = new System.Drawing.Point(67, 375);
             this.btnAddListCountry.Name = "btnAddListCountry";
             this.btnAddListCountry.Size = new System.Drawing.Size(125, 33);
             this.btnAddListCountry.TabIndex = 3;
@@ -74,25 +75,34 @@
             // 
             // btnAddContractor
             // 
-            this.btnAddContractor.Location = new System.Drawing.Point(390, 283);
+            this.btnAddContractor.Location = new System.Drawing.Point(390, 375);
             this.btnAddContractor.Name = "btnAddContractor";
             this.btnAddContractor.Size = new System.Drawing.Size(125, 33);
             this.btnAddContractor.TabIndex = 4;
             this.btnAddContractor.Text = "Add Contractor";
             this.btnAddContractor.UseVisualStyleBackColor = true;
+            this.btnAddContractor.Click += new System.EventHandler(this.btnAddContractor_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 282);
+            this.textBox1.Location = new System.Drawing.Point(67, 349);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(272, 20);
             this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(390, 349);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(272, 20);
+            this.textBox2.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnAddContractor);
             this.Controls.Add(this.btnAddListCountry);
@@ -114,6 +124,7 @@
         private System.Windows.Forms.Button btnAddListCountry;
         private System.Windows.Forms.Button btnAddContractor;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- ListInListSecondChance/ListInListSecondChance/Form1.cs
+++ ListInListSecondChance/ListInListSecondChance/Form1.cs
@@ -28,6 +28,7 @@ namespace ListInListSecondChance
             if (listBox2.Items.Count > 0)
             {
                 listBox2.Items.Clear();
+                return;
             }
 
             if (listBox1.SelectedIndex == 0)
@@ -36,6 +37,7 @@ namespace ListInListSecondChance
                 {
                     listBox2.Items.Add(item);
                 }
+                return;
             }
 
             if (listBox1.SelectedIndex == 1)
@@ -44,6 +46,7 @@ namespace ListInListSecondChance
                 {
                     listBox2.Items.Add(item);
                 }
+                return;
             }
 
             if (listBox1.SelectedIndex == 2)
@@ -52,13 +55,14 @@ namespace ListInListSecondChance
                 {
                     listBox2.Items.Add(item);
                 }
+                return;
             }
         }
         public void AddCountry()
         {
-            CountryList.Add("Poland");
-            CountryList.Add("Italy");
-            CountryList.Add("Hungary");
+            CountryList.Add("POLAND");
+            CountryList.Add("ITALY");
+            CountryList.Add("HUNGARY");
         }
         public void AddContractors()
         {
@@ -83,20 +87,40 @@ namespace ListInListSecondChance
 
         private void btnAddListCountry_Click(object sender, EventArgs e)
         {
+            string newContractor = textBox1.Text.ToUpper();
+
             if (textBox1.Text == "")
             {
                 MessageBox.Show("Nie podano kraju!", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-            if (listBox1.Items.Contains(textBox1.Text))
+            if (listBox1.Items.Contains(newContractor))
             {
                 MessageBox.Show("Istnieje juz taki kraj!", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
-            if (textBox1.Text != "")
+
+            if (textBox1.Text != "" && !listBox1.Items.Contains(newContractor))
             {
-                string lista = textBox1.Text;
-                CountryList.Add(lista);
+                CountryList.Add(newContractor);
                 listBox1.Items.Clear();
                 AddCountryListToTheListBox1();
+                return;
+            }
+        }
+
+        private void btnAddContractor_Click(object sender, EventArgs e)
+        {
+            List<string> SelectedCountry = list<string> listBox1.SelectedItem;
+
+            if (listBox1.SelectedItem == null)
+            {
+                return;
+            }
+            else
+            {
+                listBox2.Items.Add(textBox2.Text);
+                return;
             }
         }
     }
