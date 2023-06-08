ae921cb1e2e94c033123102dc4a6d077deeabc67
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-06-08 17:29:47 +0200

    CV project

---
+++ DynamicListProject/DynamicListProject/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DynamicListProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnAddCountry = new System.Windows.Forms.Button();
+            this.btnAddContractor = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Coral;
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.btnAddContractor);
+            this.tabPage1.Controls.Add(this.btnAddCountry);
+            this.tabPage1.Controls.Add(this.listBox2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "First";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Coral;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Second";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(35, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 173);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(521, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(238, 173);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btnAddCountry
+            // 
+            this.btnAddCountry.Location = new System.Drawing.Point(35, 244);
+            this.btnAddCountry.Name = "btnAddCountry";
+            this.btnAddCountry.Size = new System.Drawing.Size(126, 34);
+            this.btnAddCountry.TabIndex = 2;
+            this.btnAddCountry.Text = "Dodaj Kraj";
+            this.btnAddCountry.UseVisualStyleBackColor = true;
+            this.btnAddCountry.Click += new System.EventHandler(this.btnAddCountry_Click);
+            // 
+            // btnAddContractor
+            // 
+            this.btnAddContractor.Location = new System.Drawing.Point(521, 244);
+            this.btnAddContractor.Name = "btnAddContractor";
+            this.btnAddContractor.Size = new System.Drawing.Size(126, 34);
+            this.btnAddContractor.TabIndex = 3;
+            this.btnAddContractor.Text = "Dodaj Kontrahenta";
+            this.btnAddContractor.UseVisualStyleBackColor = true;
+            this.btnAddContractor.Click += new System.EventHandler(this.btnAddContractor_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(35, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(521, 207);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(238, 31);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Moto-Project";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddContractor;
+        private System.Windows.Forms.Button btnAddCountry;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DynamicListProject/DynamicListProject/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicListProject
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, List<string>> CountryList = new Dictionary<string, List<string>>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddCountry_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nameList = textBox1.Text.ToUpper();
+
+                if (nameList == string.Empty)
+                {
+                    MessageBox.Show("Nie wpisano kraju", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                if (CountryList.ContainsKey(nameList))
+                {
+                    MessageBox.Show("Kraj juz istnieje", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (nameList != string.Empty && !CountryList.ContainsKey(nameList))
+                {
+                    List<string> country = new List<string>();
+                    CountryList.Add(nameList, country);
+                    listBox1.Items.Clear();
+                    DisplayCountriesInListBoxOne();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DisplayCountriesInListBoxOne()
+        {
+            foreach (string country in CountryList.Keys)
+            {
+                listBox1.Items.Add(country);
+            }
+        }
+
+        private void btnAddContractor_Click(object sender, EventArgs e)
+        {
+            //Nad tym pracowac
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedCountry = listBox1.SelectedItem.ToString();
+                string contractorName = textBox2.Text;
+
+                if (!string.IsNullOrEmpty(contractorName))
+                {
+                    if (CountryList.ContainsKey(selectedCountry))
+                    {
+                        List<string> contractors = CountryList[selectedCountry];
+                        contractors.Add(contractorName);
+                        listBox2.Items.Clear();
+                        listBox2.Items.AddRange(contractors.ToArray());
+                        textBox2.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nie podano nazwy kontrahenta", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
