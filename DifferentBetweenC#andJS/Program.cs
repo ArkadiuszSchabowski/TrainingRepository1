508108bdb7469efe0ed0e9a769ce63c624beeb86
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-07-17 23:28:38 +0200

    I understood hermetization and new keyword value.

--- DifferentBetweenC#andJS/Program.cs
+++ DifferentBetweenC#andJS/Program.cs
@@ -10,11 +10,24 @@ namespace DifferentBetweenC_andJS
     {
         static void Main(string[] args)
         {
-            var number = 5;
-            Console.WriteLine(number);
-            number = 8;
-            Console.WriteLine(number);
-            Console.ReadKey();
+
+            //Jezyk C# jest statycznie typowany, dlatego nie pozwoli na taka akcje!
+            // Inaczej wyrzuci nam blad. W JS jest to jak najbardziej akceptowane.
+
+            //var number = 5;
+            //Console.WriteLine(number);
+            //number = "Pies";
+            //Console.WriteLine(number);
+            //Console.ReadKey();
+
+            // Rowniez jesli chcecmy zadeklarowac zmienna typu var musimy od razu ja zainicjalizowac!
+            // Inaczej wyrzuci nam blad. W JS jest to jak najbardziej akceptowane.
+
+            //var text;
+
+            //Zmienna w C# NIE moze zaczynac sie znakiem dolara!
+
+            //string $data = "jakis string";
         }
     }
 }
---
+++ Herm/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Dominika";
+            person.Name = "-";
+            Console.WriteLine(person.Name);
+
+            person.Age = 21;
+            person.Age = -5;
+            person.Age = 205;
+            Console.WriteLine(person.Age);
+            Console.ReadLine();
+        }
+    }
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return "Imie uzytkownika to " + name;
+            }
+            set
+            {
+                if (value.Length > 2)
+                    name = value;
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0 && value < 200)
+                age = value;
+            }
+        }
+
+    }
+}
