fa77a342e539ab4502a94d958e07359da4485fe7
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-07-21 16:58:30 +0200

    Some differents

--- DifferentBetweenC#andJS/Program.cs
+++ DifferentBetweenC#andJS/Program.cs
@@ -30,36 +30,54 @@ namespace DifferentBetweenC_andJS
             //string $data = "jakis string";
 
             //Switch w JS
-            //W instrukcji warunkowej switch w JS warunku default nie trzeba konczyc slowkiem kluczowym break. W C# to niedopuszczalne.
+            //W instrukcji warunkowej switch w JS warunku default nie trzeba konczyc slowkiem kluczowym break. W C# jest to niedopuszczalne.
 
-            Console.WriteLine("What is the your favorite animal?");
-            Console.WriteLine("A - guinea pig");
-            Console.WriteLine("B - dog");
-            Console.WriteLine("C - cat");
-            Console.WriteLine("D - rabbit");
+            //Console.WriteLine("What is the your favorite animal?");
+            //Console.WriteLine("A - guinea pig");
+            //Console.WriteLine("B - dog");
+            //Console.WriteLine("C - cat");
+            //Console.WriteLine("D - rabbit");
 
-            string question = Console.ReadLine();
-            string text = "Your favorite animal is a ";
+            //string question = Console.ReadLine();
+            //string text = "Your favorite animal is a ";
 
-            switch (question)
-            {
-                case "A":
-                    Console.WriteLine(text + "guinea pig!");
-                    break;
-                case "B":
-                    Console.WriteLine(text + "dog!");
-                    break;
-                case "C":
-                    Console.WriteLine(text + "cat!");
-                    break;
-                case "D":
-                    Console.WriteLine(text + "rabbit!");
-                    break;
-                default:
-                    Console.WriteLine(text + "other animal!");
-                    break;
-            }
-            Console.ReadKey();
+            //switch (question)
+            //{
+            //    case "A":
+            //        Console.WriteLine(text + "guinea pig!");
+            //        break;
+            //    case "B":
+            //        Console.WriteLine(text + "dog!");
+            //        break;
+            //    case "C":
+            //        Console.WriteLine(text + "cat!");
+            //        break;
+            //    case "D":
+            //        Console.WriteLine(text + "rabbit!");
+            //        break;
+            //    default:
+            //        Console.WriteLine(text + "other animal!");
+            //        break;
+            //}
+            //Console.ReadKey();
+
+            //W JS nie ma wsparcia dla poliformizmu statycznego. Dlatego w przypadku takiego kodu wykona się druga funkcja, a pierwsza będzie ignorowana:
+
+            //    <script>
+            //        function Add(a, b)
+            //        {
+            //            let c = a + b;
+            //            console.log("Wynik: " + c);
+            //        }
+            //        function Add(d, e, f)
+            //        {
+            //            let g = d + e + f;
+            //            console.log("Wynik: " + g);
+            //        }
+            //        Add(1, 5);
+            //        Add(1, 2, 5);
+            //</ script >
+
         }
     }
 }
