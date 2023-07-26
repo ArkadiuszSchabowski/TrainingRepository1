fdf1af1d7357254d7d67e6916690642a179ed786
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-07-26 23:18:07 +0200

    arrays in JS

---
+++ ValueTypeVSReferenceType/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeVSReferenceType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+            int result = Addition(5, 10);
+            Console.WriteLine(result);
+            Substraction(a, b);
+            Console.WriteLine();
+            Console.WriteLine($"a: {a} + b: {b}");
+            Console.ReadKey();
+        }
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Substraction(int a, int b)
+        {
+
+        }
+    }
+}
