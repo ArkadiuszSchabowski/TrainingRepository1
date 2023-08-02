160c6c2f716a50e93eef5e79f43ecc6770e21a67
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-08-02 21:14:05 +0200

    Milioners in C#!

--- MilionersOnWindowsForm_TestOwnSkills/Form1.cs
+++ MilionersOnWindowsForm_TestOwnSkills/Form1.cs
@@ -16,6 +16,16 @@ namespace MilionersOnWindowsForm_TestOwnSkills
             AssignQuestionsToArray();
             DisplayQuestions(_currentQuestion);
         }
+        private void AssignAccountBalanceToUser()
+        {
+            lblAccountBalance.Text = _accountBalance.ToString();
+        }
+        private void AssignQuestionsToArray()
+        {
+            _arrayQuestions[0] = "Jakiego koloru jest świnka Pepa?";
+            _arrayQuestions[1] = "Co jedzą świnki, by ścierać zęby?";
+            _arrayQuestions[2] = "Jakie jest prawidłowe poidełko dla jednej świnki morskiej?";
+        }
 
         private void AssignButtonsToQuestion(int currentQuestion)
         {
@@ -28,10 +38,10 @@ namespace MilionersOnWindowsForm_TestOwnSkills
                     btnD.Text = "D.Czarna";
                     break;
                 case 1:
-                    btnA.Text = "Sałatę rzymską";
-                    btnB.Text = "Siano";
-                    btnC.Text = "Banana";
-                    btnD.Text = "Pomidora";
+                    btnA.Text = "A.Sałatę rzymską";
+                    btnB.Text = "B.Siano";
+                    btnC.Text = "C.Banana";
+                    btnD.Text = "D.Pomidora";
                     break;
                 case 2:
                     btnA.Text = "A.125ml lub mniejsze";
@@ -44,57 +54,82 @@ namespace MilionersOnWindowsForm_TestOwnSkills
 
         }
 
-        private void AssignQuestionsToArray()
+
+        private void DisplayQuestions(int currentQuestion)
         {
-            _arrayQuestions[0] = "Jakiego koloru jest świnka Pepa?";
-            _arrayQuestions[1] = "Co jedzą świnki, by ścierać zęby?";
-            _arrayQuestions[2] = "Jakie jest prawidłowe poidełko dla jednej świnki morskiej?";
+            if (_currentQuestion < _arrayQuestions.Length)
+            {
+                lblQuestion.Text = _arrayQuestions[_currentQuestion];
+                AssignButtonsToQuestion(_currentQuestion);
+            }
         }
 
-        private void DisplayQuestions(int questionNumber)
+        private void CheckAnswer(string userChoice)
         {
-            lblQuestion.Text = _arrayQuestions[0];
-            AssignButtonsToQuestion(_currentQuestion);
-
             if (_currentQuestion == 0)
             {
                 if (_userChoice == "A.Biała")
                 {
                     _userChoice = "";
-                    lblQuestion.Text = _arrayQuestions[1];
                     _currentQuestion++;
+                    _accountBalance += 100;
+                    AssignAccountBalanceToUser();
+
                     DisplayQuestions(_currentQuestion);
                     AssignButtonsToQuestion(_currentQuestion);
+
+                    return;
                 }
                 else if (_userChoice == "B.Różowa" || _userChoice == "C.Szara" || _userChoice == "D.Czarna")
                 {
                     MessageBox.Show("Dziękujemy za grę otrzymujesz nagrodę pocieszenia\nŚwiński kubek jest Twój!");
                     EndGame();
-                }
-
-                if (_currentQuestion == 1)
-                {
-
-                    if (_userChoice == "A.Biała")
-                    {
-                        _userChoice = "";
-                        lblQuestion.Text = _arrayQuestions[1];
-                        _currentQuestion++;
-                        DisplayQuestions(_currentQuestion);
-                        AssignButtonsToQuestion(_currentQuestion);
-
-                    }
-                    else if (_userChoice == "B.Różowa" || _userChoice == "C.Szara" || _userChoice == "D.Czarna")
-                    {
-                        MessageBox.Show("Dziękujemy za grę otrzymujesz nagrodę pocieszenia\nŚwiński kubek jest Twój!");
-                        EndGame();
-                    }
+                    return;
                 }
             }
-        }
-        private void CheckAnswer(string userChoice)
-        {
 
+            if (_currentQuestion == 1)
+            {
+
+                if (_userChoice == "B.Siano")
+                {
+                    _userChoice = "";
+                    lblQuestion.Text = _arrayQuestions[1];
+                    _accountBalance += 100;
+                    _currentQuestion++;
+                    DisplayQuestions(_currentQuestion);
+                    AssignButtonsToQuestion(_currentQuestion);
+                    return;
+                }
+                else if (_userChoice == "A.Sałatę rzymską" || _userChoice == "C.Banana" || _userChoice == "D.Pomidora")
+                {
+                    MessageBox.Show("Dziękujemy za grę otrzymujesz nagrodę pocieszenia\nŚwiński kubek jest Twój!");
+                    EndGame();
+                    return;
+                }
+            }
+            if (_currentQuestion == 2)
+            {
+
+                if (_userChoice == "D.250ml lub większe")
+                {
+                    //_userChoice = "";
+                    //lblQuestion.Text = _arrayQuestions[1];
+                    //_currentQuestion++;
+                    //DisplayQuestions(_currentQuestion);
+                    //AssignButtonsToQuestion(_currentQuestion);
+
+                    _accountBalance += 100;
+                    MessageBox.Show($"Congrats! You win a {_accountBalance}$!");
+                    EndGame();
+
+                }
+                else if (_userChoice == "A.125ml lub mniejsze" || _userChoice == "B.200ml" || _userChoice == "C.225ml")
+                {
+                    MessageBox.Show("Dziękujemy za grę otrzymujesz nagrodę pocieszenia\nŚwiński kubek jest Twój!");
+                    EndGame();
+                }
+            }
         }
 
         private void EndGame()
@@ -103,10 +138,6 @@ namespace MilionersOnWindowsForm_TestOwnSkills
         }
 
 
-        private void AssignAccountBalanceToUser()
-        {
-            lblAccountBalance.Text = _accountBalance.ToString();
-        }
 
         private void btnA_Click(object sender, EventArgs e)
         {
@@ -118,16 +149,19 @@ namespace MilionersOnWindowsForm_TestOwnSkills
         private void btnB_Click(object sender, EventArgs e)
         {
             _userChoice = (sender as Button).Text;
+            CheckAnswer(_userChoice);
         }
 
         private void btnC_Click(object sender, EventArgs e)
         {
             _userChoice = (sender as Button).Text;
+            CheckAnswer(_userChoice);
         }
 
         private void btnD_Click(object sender, EventArgs e)
         {
             _userChoice = (sender as Button).Text;
+            CheckAnswer(_userChoice);
         }
     }
 }
