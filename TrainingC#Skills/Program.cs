61c13c899e53ebcd742e56787b2732ad969abcbe
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-08-11 12:49:38 +0200

    Some training in C#

---
+++ TrainingC#Skills/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingC_Skills
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 2;
+            string userAge = (age < 18) ? "Użytkownik ma mniej niż 18 lat" : "Użytkownik ma ponad 18 lat";
+            Console.WriteLine(userAge);
+            Console.ReadLine();
+        }
+    }
+}
